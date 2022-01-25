c756606aee55ab0b96ca3096c5b5ef27789bfd23
duck59uet <mavuonguron@gmail.com>
2022-01-25 14:58:00 +0700

    Update updateManifest.sh

--- ci/updateManifest.sh
+++ ci/updateManifest.sh
@@ -3,37 +3,15 @@ set -xe
 
 apk add bash && apk add git && apk add --update curl && rm -rf /var/cache/apk/*
 
-# config git
-mkdir -p ~/.ssh
-echo "${SSH_KEY_GITOPS}" >> id_rsa
-echo '-----BEGIN RSA PRIVATE KEY-----' >> ~/.ssh/id_rsa
-sed -e "s/\S\{64\}/&\n/g" id_rsa >> ~/.ssh/id_rsa
-echo '-----END RSA PRIVATE KEY-----' >> ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-ssh-keyscan -H 'gitlab.com' >> ~/.ssh/known_hosts
-
 # clone repo manifest
-git clone ${REPO_MANIFEST_URL}
+git clone "https://${PERSONAL_ACCESS_KEY}@${REPO_MANIFEST_URL}"
 cd ./${REPO_MANIFEST_NAME}
 git checkout ${REPO_MANIFEST_BRANCH} && git pull
 
-if [ ${CI_COMMIT_REF_NAME} = "master" ]
+if [ ${GITHUB_REF_NAME} = "main" ]
 then
-  echo 'This is master branch'
-elif [ ${CI_COMMIT_REF_NAME} = "develop" ]
-then
-  echo 'This is develop branch'
-  cd ${REPO_MANIFEST_ENV_DEV}
-elif [ ${CI_COMMIT_REF_NAME} = "qa" ]
-then
-  echo 'This is qa branch'
-  cd ${REPO_MANIFEST_ENV_QA}
-elif [ ${CI_COMMIT_REF_NAME} = "uat" ]
-then
-  echo 'This is uat branch'
-  cd ${REPO_MANIFEST_ENV_UAT}
+  echo 'This is main branch'
+  cd ${REPO_MANIFEST_ENV_MAIN}
 else
   exit
 fi
@@ -43,8 +21,7 @@ curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack
 ./kustomize edit set image ${REPO_MANIFEST_TAG_IMAGE}=${CONTAINER_RELEASE_IMAGE}
 rm kustomize
 
-git config --global user.email "${GITLAB_USER_EMAIL}"
-git config --global user.name "${GITLAB_USER_NAME}"
+git config --global user.name "${GITHUB_ACTOR}"
 git add . 
 git commit -m "Update image to ${CONTAINER_RELEASE_IMAGE}"
 git push
