776c5fc20ed5b3d4b5921045cc79427809a8ce9b
duck59uet <mavuonguron@gmail.com>
2022-01-25 15:35:36 +0700

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -6,8 +6,6 @@ echo $GITHUB_PASSWORD | docker login ghcr.io -u $GITHUB_USERNAME --password-stdi
 if [ ${GITHUB_REF_NAME} = "main" ]
 then
     echo 'This is main branch'
-    cp ./src/robots/robots-aura.txt ./src/robots.txt
-    cp ./src/robots/sitemap-aura.xml ./src/sitemap.xml
 fi
 #Build and push image
 docker build -t ${CONTAINER_RELEASE_IMAGE} -f Dockerfile .
