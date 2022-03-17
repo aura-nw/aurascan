d7909f5b4ccf37d9349e9f271a13234a8e1e38f8
nttnguyen136 <nttnguyen136@gmail.com>
2022-03-17 17:15:02 +0700

    add chart

--- src/app/shared/components/wallet-connect/wallet-connect.module.ts
+++ src/app/shared/components/wallet-connect/wallet-connect.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { WalletConnectComponent } from "./wallet-connect.component";
 import { WalletListComponent } from "./wallet-list/wallet-list.component";
 import { WalletDetailComponent } from './wallet-detail/wallet-detail.component';
+import { NgApexchartsModule } from "ng-apexcharts";
 
 @NgModule({
   declarations: [WalletConnectComponent, WalletListComponent, WalletDetailComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, NgApexchartsModule],
   exports: [WalletConnectComponent],
 })
 export class WalletConnectModule {}
--- src/app/shared/components/wallet-connect/wallet-detail/wallet-detail.component.ts
+++ src/app/shared/components/wallet-connect/wallet-detail/wallet-detail.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ChartComponent } from "ng-apexcharts";
+
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart,
+  ApexFill,
+  ApexDataLabels,
+  ApexLegend,
+} from "ng-apexcharts";
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+  fill: ApexFill;
+  legend: ApexLegend;
+  dataLabels: ApexDataLabels;
+};
 
 @Component({
-  selector: 'app-wallet-detail',
-  templateUrl: './wallet-detail.component.html',
-  styleUrls: ['./wallet-detail.component.scss']
+  selector: "app-wallet-detail",
+  templateUrl: "./wallet-detail.component.html",
+  styleUrls: ["./wallet-detail.component.scss"],
 })
 export class WalletDetailComponent implements OnInit {
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.chartOptions = {
+      series: [44,2],
+      chart: {
+        width: 380,
+        type: "donut",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      fill: {
+        type: "gradient",
+      },
+      legend: {
+        formatter: function (val, opts) {
+          return val + " - " + opts.w.globals.series[opts.seriesIndex];
+        },
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200,
+            },
+            legend: {
+              position: "bottom",
+            },
+          },
+        },
+      ],
+    };
   }
 
+  ngOnInit(): void {}
 }
