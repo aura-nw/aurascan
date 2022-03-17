f4604fb9158b926293e77f58193fc3f635983acc
nttnguyen136 <nttnguyen136@gmail.com>
2022-03-17 16:54:03 +0700

    add wallet detail

--- src/app/shared/components/wallet-connect/wallet-connect.module.ts
+++ src/app/shared/components/wallet-connect/wallet-connect.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { WalletConnectComponent } from "./wallet-connect.component";
-import { WalletListComponent } from "./wallet-list/wallet-list/wallet-list.component";
+import { WalletListComponent } from "./wallet-list/wallet-list.component";
+import { WalletDetailComponent } from './wallet-detail/wallet-detail.component';
 
 @NgModule({
-  declarations: [WalletConnectComponent, WalletListComponent],
+  declarations: [WalletConnectComponent, WalletListComponent, WalletDetailComponent],
   imports: [CommonModule],
   exports: [WalletConnectComponent],
 })
---
+++ src/app/shared/components/wallet-connect/wallet-detail/wallet-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-detail',
+  templateUrl: './wallet-detail.component.html',
+  styleUrls: ['./wallet-detail.component.scss']
+})
+export class WalletDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
