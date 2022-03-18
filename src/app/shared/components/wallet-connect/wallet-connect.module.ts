a877b5a44b3ce350cf6e10d386a783a80dc5d96a
nttnguyen136 <nttnguyen136@gmail.com>
2022-03-18 16:49:28 +0700

    add user menu

--- src/app/shared/components/wallet-connect/wallet-connect.module.ts
+++ src/app/shared/components/wallet-connect/wallet-connect.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { WalletConnectComponent } from "./wallet-connect.component";
 import { WalletListComponent } from "./wallet-list/wallet-list.component";
-import { WalletDetailComponent } from './wallet-detail/wallet-detail.component';
+import { WalletDetailComponent } from "./wallet-detail/wallet-detail.component";
 import { NgApexchartsModule } from "ng-apexcharts";
+import { MatMenuModule } from "@angular/material/menu";
+import { FeatherModule } from "angular-feather";
 
 @NgModule({
-  declarations: [WalletConnectComponent, WalletListComponent, WalletDetailComponent],
-  imports: [CommonModule, NgApexchartsModule],
+  declarations: [
+    WalletConnectComponent,
+    WalletListComponent,
+    WalletDetailComponent,
+  ],
+  imports: [CommonModule, NgApexchartsModule, MatMenuModule, FeatherModule],
   exports: [WalletConnectComponent],
 })
 export class WalletConnectModule {}
