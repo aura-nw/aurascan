455f1443db298b9844c23931a56ecdd6d4b2193e
nttnguyen136 <nttnguyen136@gmail.com>
2022-03-21 15:42:10 +0700

    calculate wallet balances

--- src/app/shared/components/wallet-connect/wallet-detail/wallet-detail.component.ts
+++ src/app/shared/components/wallet-connect/wallet-detail/wallet-detail.component.ts
@@ -1,34 +1,12 @@
 import { Component, Input, OnInit, ViewChild } from "@angular/core";
-import {
-  ApexPlotOptions,
-  ApexStates,
-  ApexTooltip,
-  ChartComponent,
-} from "ng-apexcharts";
+import { ChartComponent } from "ng-apexcharts";
 
-import {
-  ApexNonAxisChartSeries,
-  ApexResponsive,
-  ApexChart,
-  ApexFill,
-  ApexDataLabels,
-  ApexLegend,
-} from "ng-apexcharts";
 import { WalletService } from "src/app/core/services/wallet.service";
-
-export type ChartOptions = {
-  series: ApexNonAxisChartSeries;
-  chart: ApexChart;
-  responsive: ApexResponsive[];
-  labels: any;
-  fill: ApexFill;
-  legend: ApexLegend;
-  dataLabels: ApexDataLabels;
-  plotOptions: ApexPlotOptions;
-  colors: string[];
-  states: ApexStates;
-  tooltip: ApexTooltip;
-};
+import {
+  chartCustomOptions,
+  ChartOptions,
+  CHART_OPTIONS,
+} from "./wallet-chart-option";
 
 @Component({
   selector: "app-wallet-detail",
@@ -47,117 +25,13 @@ export class WalletDetailComponent implements OnInit {
     stackedAmount: 0,
   };
 
-  chartCustomOptions: { name: string; color: string }[] = [
-    {
-      name: "Available",
-      color: "#11cdef",
-    },
-    {
-      name: "Staked",
-      color: "#5e72e4",
-    },
-  ];
+  chartCustomOptions = chartCustomOptions;
 
   constructor(private walletService: WalletService) {
-    this.chartOptions = {
-      series: [0, 0],
-      labels: this.chartCustomOptions.map((e) => e.name).concat("Total"),
-      colors: this.chartCustomOptions.map((e) => e.color).concat("#ced4da"),
-      dataLabels: {
-        enabled: false,
-      },
-      chart: {
-        width: 280,
-        type: "donut",
-      },
-      states: {
-        hover: {
-          filter: {
-            type: "none",
-          },
-        },
-        active: {
-          filter: {
-            type: "none",
-          },
-        },
-      },
-      tooltip: {
-        enabled: false,
-      },
-      plotOptions: {
-        pie: {
-          startAngle: 180,
-          endAngle: -180,
-          expandOnClick: false,
-          offsetX: 0,
-          offsetY: 0,
-          customScale: 1,
-          dataLabels: {
-            offset: 0,
-            minAngleToShowLabel: 1,
-          },
-          donut: {
-            size: "85%",
-            labels: {
-              show: true,
-              total: {
-                show: true,
-                showAlways: true,
-                label: "Total Balance",
-                fontSize: "18px",
-                fontFamily: "Helvetica, Arial, sans-serif",
-                fontWeight: 600,
-                color: "#373d3f",
-                formatter: function (w) {
-                  return w.globals.seriesTotals.reduce(
-                    (
-                      previousValue: number,
-                      currentValue: number,
-                      index: number
-                    ) => {
-                      if (index === 2) {
-                        return previousValue;
-                      }
-                      return previousValue + currentValue;
-                    },
-                    0
-                  );
-                },
-              },
-            },
-          },
-        },
-      },
-      legend: {
-        show: false,
-      },
-      responsive: [
-        {
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 200,
-            },
-            plotOptions: {
-              pie: {
-                donut: {
-                  labels: {
-                    total: {
-                      fontSize: "14px",
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      ],
-    };
+    this.chartOptions = CHART_OPTIONS;
   }
 
   ngOnInit(): void {
-    this.chartOptions.series = [0, 0, 100];
     this.walletService.getBalances(this.address).subscribe((response) => {
       if (!response) {
         return;
@@ -167,12 +41,14 @@ export class WalletDetailComponent implements OnInit {
       } else {
         const { data } = response;
 
-        if (+data.balance === 0) {
-          this.chartOptions.series = [0, 0, 100];
-        } else {
-          this.chartOptions.series = [+data.balance, data.delegated];
+        if (+data.balance !== 0) {
+          this.setChartBalances(data.balance, data.delegated);
         }
       }
     });
+  }
+
+  setChartBalances(amount: number | string, staking: number | string): void {
+    this.chartOptions.series = [+amount, +staking, 0];
   }
 }
