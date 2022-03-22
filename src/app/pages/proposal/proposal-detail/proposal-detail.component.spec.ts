df5117ef83bee7df6ff99aa2deeb19b8155c9912
letrieuthien <ltthien001@gmail.com>
2022-03-22 09:41:08 +0700

    .

---
+++ src/app/pages/proposal/proposal-detail/proposal-detail.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProposalDetailComponent } from './proposal-detail.component';
+
+describe('ProposalDetailComponent', () => {
+  let component: ProposalDetailComponent;
+  let fixture: ComponentFixture<ProposalDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProposalDetailComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProposalDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/proposal/proposal-detail/proposal-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proposal-detail',
+  templateUrl: './proposal-detail.component.html',
+  styleUrls: ['./proposal-detail.component.scss']
+})
+export class ProposalDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
