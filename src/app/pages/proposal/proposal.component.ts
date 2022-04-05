58899380866b59da36510571a2696dd46314270f
nhphuc2411 <phucnguyen241193@gmail.com>
2022-04-05 11:17:48 +0700

    [PhucNH10] Mapping API proposal detail

--- src/app/pages/proposal/proposal.component.ts
+++ src/app/pages/proposal/proposal.component.ts
@@ -1,19 +1,19 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatDialog } from "@angular/material/dialog";
-import { PageEvent } from "@angular/material/paginator";
-import { MatSort } from "@angular/material/sort";
-import { MatTableDataSource } from "@angular/material/table";
+import { MatDialog } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { DATEFORMAT } from '../../core/constants/common.constant';
 import { PROPOSAL_STATUS, PROPOSAL_VOTE } from '../../core/constants/status.constant';
-import { ResponseDto, TableTemplate } from "../../core/models/common.model";
-import { ProposalService } from "../../core/services/proposal.service";
-import { ProposalVoteComponent } from "./proposal-vote/proposal-vote.component";
+import { ResponseDto, TableTemplate } from '../../core/models/common.model';
+import { ProposalService } from '../../core/services/proposal.service';
+import { ProposalVoteComponent } from './proposal-vote/proposal-vote.component';
 
 @Component({
   selector: 'app-proposal',
   templateUrl: './proposal.component.html',
-  styleUrls: ['./proposal.component.scss']
+  styleUrls: ['./proposal.component.scss'],
 })
 export class ProposalComponent implements OnInit {
   statusConstant = PROPOSAL_STATUS;
@@ -29,7 +29,7 @@ export class ProposalComponent implements OnInit {
     { matColumnDef: 'status', headerCellDef: 'Status' },
     { matColumnDef: 'votingStart', headerCellDef: 'Voting Start' },
     { matColumnDef: 'submitTime', headerCellDef: 'Submit Time' },
-    { matColumnDef: 'totalDeposit', headerCellDef: 'Total Deposit' }
+    { matColumnDef: 'totalDeposit', headerCellDef: 'Total Deposit' },
   ];
   displayedColumns: string[] = this.templates.map((dta) => dta.matColumnDef);
   dataSource: MatTableDataSource<any>;
@@ -38,17 +38,10 @@ export class ProposalComponent implements OnInit {
   pageIndex = 0;
   lastedList = [];
 
-  constructor(
-    private proposalService: ProposalService,
-    public dialog: MatDialog,
-    private datePipe: DatePipe
-  ) { }
+  constructor(private proposalService: ProposalService, public dialog: MatDialog, private datePipe: DatePipe) {}
 
   ngOnInit(): void {
-    this.breadCrumbItems = [
-      { label: 'Proposal' },
-      { label: 'List', active: true }
-    ];
+    this.breadCrumbItems = [{ label: 'Proposal' }, { label: 'List', active: true }];
     this.getList();
   }
 
@@ -59,39 +52,37 @@ export class ProposalComponent implements OnInit {
   }
 
   getList(): void {
-    this.proposalService
-        .getProposal(this.pageSize, this.pageIndex)
-        .subscribe((res: ResponseDto) => {
-          this.dataSource = new MatTableDataSource<any>(res.data);
-          this.length = res.data.length;
-          this.dataSource.sort = this.sort;
-          this.lastedList = res.data;
-          this.lastedList.forEach((pro)=>{
-            const totalVoteYes = +pro.pro_votes_yes;
-            const totalVoteNo = +pro.pro_votes_no;
-            const totalVoteNoWithVeto = +pro.pro_votes_no_with_veto;
-            const totalVoteAbstain = +pro.pro_votes_abstain;
-            const totalVote = totalVoteYes + totalVoteNo + totalVoteNoWithVeto + totalVoteAbstain;
-            pro.pro_votes_yes = totalVoteYes*100/totalVote;
-            pro.pro_votes_no = totalVoteNo*100/totalVote;
-            pro.pro_votes_no_with_veto = totalVoteNoWithVeto*100/totalVote;
-            pro.pro_votes_abstain = totalVoteAbstain*100/totalVote;
-            pro.pro_vote_total = totalVote;
-            pro.pro_voting_start_time = this.datePipe.transform(pro.pro_voting_start_time, DATEFORMAT.DATETIME_UTC);
-            pro.pro_voting_end_time = this.datePipe.transform(pro.pro_voting_end_time, DATEFORMAT.DATETIME_UTC);
-            pro.pro_submit_time = this.datePipe.transform(pro.pro_submit_time, DATEFORMAT.DATETIME_UTC);
-          })
-        })
+    this.proposalService.getProposal(this.pageSize, this.pageIndex).subscribe((res: ResponseDto) => {
+      this.dataSource = new MatTableDataSource<any>(res.data);
+      this.length = res.data.length;
+      this.dataSource.sort = this.sort;
+      this.lastedList = res.data;
+      this.lastedList.forEach((pro) => {
+        const totalVoteYes = +pro.pro_votes_yes;
+        const totalVoteNo = +pro.pro_votes_no;
+        const totalVoteNoWithVeto = +pro.pro_votes_no_with_veto;
+        const totalVoteAbstain = +pro.pro_votes_abstain;
+        const totalVote = totalVoteYes + totalVoteNo + totalVoteNoWithVeto + totalVoteAbstain;
+        pro.pro_votes_yes = (totalVoteYes * 100) / totalVote;
+        pro.pro_votes_no = (totalVoteNo * 100) / totalVote;
+        pro.pro_votes_no_with_veto = (totalVoteNoWithVeto * 100) / totalVote;
+        pro.pro_votes_abstain = (totalVoteAbstain * 100) / totalVote;
+        pro.pro_vote_total = totalVote;
+        pro.pro_voting_start_time = this.datePipe.transform(pro.pro_voting_start_time, DATEFORMAT.DATETIME_UTC);
+        pro.pro_voting_end_time = this.datePipe.transform(pro.pro_voting_end_time, DATEFORMAT.DATETIME_UTC);
+        pro.pro_submit_time = this.datePipe.transform(pro.pro_submit_time, DATEFORMAT.DATETIME_UTC);
+      });
+    });
   }
 
   getStatus(key: string) {
-    let resObj: { value: string, class: string } = null;
-    const statusObj = this.statusConstant.find(s => s.key === key);
+    let resObj: { value: string; class: string } = null;
+    const statusObj = this.statusConstant.find((s) => s.key === key);
     if (statusObj !== undefined) {
       resObj = {
         value: statusObj.value,
-        class: statusObj.class
-      }
+        class: statusObj.class,
+      };
     }
     return resObj;
   }
@@ -102,26 +93,23 @@ export class ProposalComponent implements OnInit {
       highest = 0;
     }
     let key;
-    let resObj: { value: number, name: string, class: string } = null;
+    let resObj: { value: number; name: string; class: string } = null;
     if (highest === yes) {
       key = 0;
-    }
-    else if (highest === no) {
+    } else if (highest === no) {
       key = 1;
-    }
-    else if (highest === noWithVeto) {
+    } else if (highest === noWithVeto) {
       key = 2;
-    }
-    else {
+    } else {
       key = 3;
     }
-    const statusObj = this.voteConstant.find(s => s.key === key);
+    const statusObj = this.voteConstant.find((s) => s.key === key);
     if (statusObj !== undefined) {
       resObj = {
         value: highest,
         class: statusObj.class,
-        name: statusObj.value
-      }
+        name: statusObj.value,
+      };
     }
     return resObj;
   }
@@ -132,7 +120,7 @@ export class ProposalComponent implements OnInit {
       width: '600px',
       data: { id, title, voteValue: this.voteValue },
     });
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       this.voteValue = result;
     });
   }
