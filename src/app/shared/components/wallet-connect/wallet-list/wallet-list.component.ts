6058124b307477ff6c5c518d6e7304e837c22418
nttnguyen136 <nttnguyen136@gmail.com>
2022-04-27 09:35:19 +0700

    update list wallet

--- src/app/shared/components/wallet-connect/wallet-list/wallet-list.component.ts
+++ src/app/shared/components/wallet-connect/wallet-list/wallet-list.component.ts
@@ -1,20 +1,20 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
-import { WALLET_PROVIDER } from "../../../../core/constants/wallet.constant";
-import { IWalletInfo } from "../../../../core/models/wallet";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { WALLET_PROVIDER } from '../../../../core/constants/wallet.constant';
+import { IWalletInfo } from '../../../../core/models/wallet';
 @Component({
-  selector: "app-wallet-list",
-  templateUrl: "./wallet-list.component.html",
-  styleUrls: ["./wallet-list.component.scss"],
+  selector: 'app-wallet-list',
+  templateUrl: './wallet-list.component.html',
+  styleUrls: ['./wallet-list.component.scss'],
 })
 export class WalletListComponent {
   @Input() walletList: IWalletInfo[] = [
     {
-      name: WALLET_PROVIDER.KEPLR,
-      icon: '../../../../../../assets/images/icon-keplr.svg',
-    },
-    {
       name: WALLET_PROVIDER.COIN98,
       icon: '../../../../../../assets/images/icon-coin98.svg',
+    },
+    {
+      name: WALLET_PROVIDER.KEPLR,
+      icon: '../../../../../../assets/images/icon-keplr.svg',
     },
   ];
 
