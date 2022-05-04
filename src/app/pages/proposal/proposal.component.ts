d2ceb65416e7a99b0575744a00a623f613c796d7
nhphuc2411 <phucnguyen241193@gmail.com>
2022-05-04 10:50:29 +0700

    [PhucNH10] check null data

--- src/app/pages/proposal/proposal.component.ts
+++ src/app/pages/proposal/proposal.component.ts
@@ -74,38 +74,40 @@ export class ProposalComponent implements OnInit {
 
   getList(): void {
     this.proposalService.getProposal().subscribe((res) => {
-      this.dataSource = new MatTableDataSource<any>(res.data);
-      this.length = res.data.length;
-      this.dataSource.sort = this.sort;
-      this.lastedList = [...res.data];
-      this.lastedList.forEach((pro, index) => {
-        pro.pro_voting_start_time = this.datePipe.transform(pro.pro_voting_start_time, DATEFORMAT.DATETIME_UTC);
-        pro.pro_voting_end_time = this.datePipe.transform(pro.pro_voting_end_time, DATEFORMAT.DATETIME_UTC);
-        pro.pro_submit_time = this.datePipe.transform(pro.pro_submit_time, DATEFORMAT.DATETIME_UTC);
-        pro.pro_total_deposits = balanceOf(pro.pro_total_deposits);
+      if (res?.data) {
+        this.dataSource = new MatTableDataSource<any>(res.data);
+        this.length = res.data.length;
+        this.dataSource.sort = this.sort;
+        this.lastedList = [...res.data];
+        this.lastedList.forEach((pro, index) => {
+          pro.pro_voting_start_time = this.datePipe.transform(pro.pro_voting_start_time, DATEFORMAT.DATETIME_UTC);
+          pro.pro_voting_end_time = this.datePipe.transform(pro.pro_voting_end_time, DATEFORMAT.DATETIME_UTC);
+          pro.pro_submit_time = this.datePipe.transform(pro.pro_submit_time, DATEFORMAT.DATETIME_UTC);
+          pro.pro_total_deposits = balanceOf(pro.pro_total_deposits);
 
-        if (index < 4) {
-          if (pro.pro_status !== 'PROPOSAL_STATUS_DEPOSIT_PERIOD') {
-            this.getVoteResult(pro.pro_id, index);
-            const expiredTime = new Date(pro.pro_voting_end_time).getTime() - new Date().getTime();
-            if (expiredTime < 0) {
-              this.getProposalDetailFromNode(pro.pro_id, index);
+          if (index < 4) {
+            if (pro.pro_status !== 'PROPOSAL_STATUS_DEPOSIT_PERIOD') {
+              this.getVoteResult(pro.pro_id, index);
+              const expiredTime = new Date(pro.pro_voting_end_time).getTime() - new Date().getTime();
+              if (expiredTime < 0) {
+                this.getProposalDetailFromNode(pro.pro_id, index);
+              }
             }
-          }
 
-          this.proposalVotes.push({
-            proId: +pro.pro_id,
-            vote: null,
-          });
-        }
-      });
-      this.getVotedProposal();
+            this.proposalVotes.push({
+              proId: +pro.pro_id,
+              vote: null,
+            });
+          }
+        });
+        this.getVotedProposal();
+      }
     });
   }
 
   getProposalDetailFromNode(pro_id, index) {
     this.proposalService.getProposalDetailFromNode(pro_id).subscribe((res) => {
-      if (res) {
+      if (res?.data) {
         this.lastedList[index].pro_status = res.data.status;
         this.dataSource.data[index].pro_status = res.data.status;
       }
