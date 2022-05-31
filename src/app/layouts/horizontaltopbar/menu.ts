f8eb43a01f4071d328f4dbd868faacef88a5bdf5
nhphuc2411 <phucnguyen241193@gmail.com>
2022-05-31 11:31:43 +0700

    update component name

--- src/app/layouts/horizontaltopbar/menu.ts
+++ src/app/layouts/horizontaltopbar/menu.ts
@@ -31,32 +31,32 @@ export const MENU: MenuItem[] = [
         icon: 'archive',
         link: '/proposal',
     },
+    // {
+    //     id: 6,
+    //     label: 'MENUITEMS.TOKEN.TEXT',
+    //     icon: 'user',
+    //     link: '/token',
+    //     subItems: [
+    //         {
+    //             id: 1,
+    //             label: 'CW-20 Top Tokens',
+    //             link: '/token',
+    //         },
+    //         {
+    //             id: 2,
+    //             label: 'CW-721 Top Tokens',
+    //             link: '/token/tokens-nft',
+    //         }
+    //     ]
+    // },
     {
         id: 6,
-        label: 'MENUITEMS.TOKEN.TEXT',
-        icon: 'user',
-        link: '/token',
-        subItems: [
-            {
-                id: 1,
-                label: 'CW-20 Top Tokens',
-                link: '/token',
-            },
-            {
-                id: 2,
-                label: 'CW-721 Top Tokens',
-                link: '/token/tokens-nft',
-            }
-        ]
-    },
-    {
-        id: 7,
         label: 'MENUITEMS.CONTRACT.TEXT',
         icon: 'user',
         link: '/contract',
     },
     {
-        id: 8,
+        id: 7,
         label: 'MENUITEMS.ACCOUNT.TEXT',
         icon: 'user',
         link: '',
