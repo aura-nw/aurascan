a6d1faf387bf318eab8e1db2403812a7df47aeec
nhphuc2411 <phucnguyen241193@gmail.com>
2022-05-31 11:57:01 +0700

    import module

--- src/app/pages/contracts/contracts.module.ts
+++ src/app/pages/contracts/contracts.module.ts
@@ -1,22 +1,36 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ContractsRoutingModule } from './contracts-routing.module';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { MaterialModule } from '../../app.module';
+import { PaginatorModule } from '../../shared/components/paginator/paginator.module';
+import { TableNoDataModule } from '../../shared/components/table-no-data/table-no-data.module';
 import { ContractsInformationsComponent } from './contracts-detail/contracts-informations/contracts-informations.component';
 import { ContractReadTypeComponent } from './contracts-detail/contracts-informations/info-tab/contract/contract-read-type/contract-read-type.component';
 import { ContractWriteTypeComponent } from './contracts-detail/contracts-informations/info-tab/contract/contract-write-type/contract-write-type.component';
-import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContractsListComponent } from './contracts-list/contracts-list.component';
+import { ContractsRoutingModule } from './contracts-routing.module';
 
 @NgModule({
   declarations: [
     ContractsInformationsComponent,
     ContractReadTypeComponent,
     ContractWriteTypeComponent,
+    ContractsListComponent,
   ],
   imports: [
     CommonModule,
     ContractsRoutingModule,
-    NgbPopoverModule
+    NgbPopoverModule,
+    PaginatorModule,
+    TableNoDataModule,
+    TranslateModule,
+    MatTableModule,
+    FormsModule,
+    MaterialModule,
   ],
-  providers: []
+  providers: [],
 })
-export class ContractsModule { }
+export class ContractsModule {}
