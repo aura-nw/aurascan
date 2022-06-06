058052d4020df9ef151e1cb0431d60f36f81fc58
Tam <mtamkg93@gmail.com>
2022-06-06 10:23:10 +0700

    [TamTM6] commit tab contract

--- src/app/pages/contracts/contracts-detail/contracts-detail.component.ts
+++ src/app/pages/contracts/contracts-detail/contracts-detail.component.ts
@@ -11,6 +11,7 @@ import { ContractService } from 'src/app/core/services/contract.service';
 export class ContractsDetailComponent implements OnInit {
   contractAddress: string | number;
   contractDetail: any;
+  priceToken = 0;
   constructor(private route: ActivatedRoute, private contractService: ContractService) {}
 
   ngOnInit(): void {
@@ -21,14 +22,24 @@ export class ContractsDetailComponent implements OnInit {
   getContractDetail(): void {
     this.contractService.getContractDetail(this.contractAddress).subscribe((res: ResponseDto) => {
       this.contractDetail = res?.data;
-      this.contractDetail.price = this.contractDetail.balance * 1;
+      this.contractDetail.price = this.contractDetail.balance * this.priceToken || 0;
       this.contractDetail.creator_address_format = this.contractDetail.creator_address.replace(
         this.contractDetail.creator_address.substring(20),
         '...',
       );
-      this.contractDetail.tx_hash_format = this.contractDetail.tx_hash
-        ? this.contractDetail.tx_hash.replace(this.contractDetail.tx_hash.substring(20), '...')
-        : '';
+      this.contractDetail.tx_hash_format = this.contractDetail.tx_hash.replace(
+        this.contractDetail.tx_hash.substring(6, this.contractDetail.tx_hash.length - 6),
+        '...',
+      );
     });
+  }
+
+  copyData(text: string): void {
+    var dummy = document.createElement('textarea');
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
   }
 }
