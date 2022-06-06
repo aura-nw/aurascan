c3ed9f359d4f6a72fd3bf30eda0c8a6dbdadfa4f
nttnguyen136 <nttnguyen136@gmail.com>
2022-06-06 11:01:53 +0700

    update data popover

--- src/app/shared/components/contract-table/contract-table.component.ts
+++ src/app/shared/components/contract-table/contract-table.component.ts
@@ -68,17 +68,30 @@ export class ContractTableComponent implements OnInit, OnChanges {
   }
 
   getPopoverData(data): IContractPopoverData {
+    /* 
+      blockHeight: 2823954
+      blockId: 3039232
+      fee: 0.00014
+      from: "aura1t0l7tjhqvspw7lnsdr9l5t8fyqpuu3jm57ezqa"
+      label: "TO"
+      method: "mint"
+      time: Thu Jun 02 2022 15:34:19 GMT+0700 (Indochina Time) {}
+      to: "aura1d96q6hrhn9287xqlg696gd2msrnffp27xd2dpft99u2pq79r28jsfrrztp"
+      txHash: "93285223BDEDEA423A615A14BD0CFB165F52C6208D075F977463B112D5E3C747"
+      value: 0
+    */
+
     return {
-      amount: 1000,
+      amount: data?.value || 0,
       code: 0,
-      fee: 0.111,
-      from_address: 'aura1tuc47nqcfr426gdynf7yqaz4u0psl5609ccsadp276vgkeykda9s96yv0z',
-      to_address: 'aura1h6r78trkk2ewrry7s3lclrqu9a22ca3hpmyqfu',
-      price: 123,
+      fee: data?.fee || 0,
+      from_address: data?.from || '-', //'aura1tuc47nqcfr426gdynf7yqaz4u0psl5609ccsadp276vgkeykda9s96yv0z',
+      to_address: data?.to || '-', //'aura1h6r78trkk2ewrry7s3lclrqu9a22ca3hpmyqfu',
+      price: 0,
       status: 'Success',
       symbol: 'AURA',
-      tokenAddress: 'aura1tuc47nqcfr426gdynf7yqaz4u0psl5609ccsadp276vgkeykda9s96yv0z',
-      tx_hash: '8B830E4B7339EB68933D40AC62E24E51867F5447D2703F6605672E44E42A8358',
+      tokenAddress: this.contractInfo?.contractsAddress,//'aura1tuc47nqcfr426gdynf7yqaz4u0psl5609ccsadp276vgkeykda9s96yv0z',
+      tx_hash: data?.txHash || '-', //'8B830E4B7339EB68933D40AC62E24E51867F5447D2703F6605672E44E42A8358',
     };
   }
 }
