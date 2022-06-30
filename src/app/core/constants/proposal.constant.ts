69cdea9277cf04666472ce47851b9bc4d4fe81da
nhphuc2411 <phucnguyen241193@gmail.com>
2022-06-30 09:30:10 +0700

    move text to constant

--- src/app/core/constants/proposal.constant.ts
+++ src/app/core/constants/proposal.constant.ts
@@ -1,61 +1,76 @@
+export enum VOTING_STATUS {
+  PROPOSAL_STATUS_REJECTED = 'PROPOSAL_STATUS_REJECTED',
+  PROPOSAL_STATUS_PASSED = 'PROPOSAL_STATUS_PASSED',
+  PROPOSAL_STATUS_VOTING_PERIOD = 'PROPOSAL_STATUS_VOTING_PERIOD',
+  PROPOSAL_STATUS_DEPOSIT_PERIOD = 'PROPOSAL_STATUS_DEPOSIT_PERIOD',
+}
+
 export const PROPOSAL_STATUS = [
-    {
-        key: 'PROPOSAL_STATUS_REJECTED',
-        value: 'rejected',
-        class: 'text--danger'
-    },
-    {
-        key: 'PROPOSAL_STATUS_PASSED',
-        value: 'passed',
-        class: 'text--primary'
-    },
-    {
-        key: 'PROPOSAL_STATUS_VOTING_PERIOD',
-        value: 'voting period',
-        class: 'text--info'
-    },
-    {
-        key: 'PROPOSAL_STATUS_DEPOSIT_PERIOD',
-        value: 'deposit period',
-        class: 'text--warning'
-    }
+  {
+    key: VOTING_STATUS.PROPOSAL_STATUS_REJECTED,
+    value: 'rejected',
+    class: 'text--danger',
+  },
+  {
+    key: VOTING_STATUS.PROPOSAL_STATUS_PASSED,
+    value: 'passed',
+    class: 'text--primary',
+  },
+  {
+    key: VOTING_STATUS.PROPOSAL_STATUS_VOTING_PERIOD,
+    value: 'voting period',
+    class: 'text--info',
+  },
+  {
+    key: VOTING_STATUS.PROPOSAL_STATUS_DEPOSIT_PERIOD,
+    value: 'deposit period',
+    class: 'text--warning',
+  },
 ];
+
+export enum VOTE_OPTION {
+  VOTE_OPTION_NULL = 'null',
+  VOTE_OPTION_UNSPECIFIED = 'VOTE_OPTION_UNSPECIFIED',
+  VOTE_OPTION_YES = 'VOTE_OPTION_YES',
+  VOTE_OPTION_ABSTAIN = 'VOTE_OPTION_ABSTAIN',
+  VOTE_OPTION_NO = 'VOTE_OPTION_NO',
+  VOTE_OPTION_NO_WITH_VETO = 'VOTE_OPTION_NO_WITH_VETO',
+}
 
 export const PROPOSAL_VOTE = [
-    {
-        key: 'VOTE_OPTION_UNSPECIFIED',
-        value: 'All',
-        class: '',
-        voteOption: ''
-    },
-    {
-        key: 'VOTE_OPTION_YES',
-        value: 'Yes',
-        class: 'primary',
-        voteOption: 'Yes'
-    },
-    {
-        key: 'VOTE_OPTION_NO',
-        value: 'No',
-        class: 'danger',
-        voteOption: 'No'
-    },
-    {
-        key: 'VOTE_OPTION_NO_WITH_VETO',
-        value: 'No with veto',
-        class: 'info',
-        voteOption: 'NoWithVeto'
-    },
-    {
-        key: 'VOTE_OPTION_ABSTAIN',
-        value: 'Abstain',
-        class: 'secondary',
-        voteOption: 'Abstain'
-    }
+  {
+    key: VOTE_OPTION.VOTE_OPTION_UNSPECIFIED,
+    value: 'All',
+    class: '',
+    voteOption: '',
+  },
+  {
+    key: VOTE_OPTION.VOTE_OPTION_YES,
+    value: 'Yes',
+    class: 'primary',
+    voteOption: 'Yes',
+  },
+  {
+    key: VOTE_OPTION.VOTE_OPTION_NO,
+    value: 'No',
+    class: 'danger',
+    voteOption: 'No',
+  },
+  {
+    key: VOTE_OPTION.VOTE_OPTION_NO_WITH_VETO,
+    value: 'No with veto',
+    class: 'info',
+    voteOption: 'NoWithVeto',
+  },
+  {
+    key: VOTE_OPTION.VOTE_OPTION_ABSTAIN,
+    value: 'Abstain',
+    class: 'secondary',
+    voteOption: 'Abstain',
+  },
 ];
 
-
 export enum MESSAGE_WARNING {
-    NOT_PARTICIPATE = 'NOT_PARTICIPATE',
-    LATE = 'LATE'
+  NOT_PARTICIPATE = 'NOT_PARTICIPATE',
+  LATE = 'LATE',
 }
