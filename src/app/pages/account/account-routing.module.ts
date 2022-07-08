58ec53f113e3e54fd3b3b8b559caff3be871f996
nttnguyen136 <nttnguyen136@gmail.com>
2022-07-08 09:46:42 +0700

    remove code

--- src/app/pages/account/account-routing.module.ts
+++ src/app/pages/account/account-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import {LayoutComponent} from "../../layouts/layout.component";
 
 const routes: Routes = [
