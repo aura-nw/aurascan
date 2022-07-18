6049f5cb878a037ac2cb673618ec43b8ab3e958a
nhphuc2411 <phucnguyen241193@gmail.com>
2022-07-18 14:31:19 +0700

    comment out token

--- src/app/layouts/horizontaltopbar/menu.ts
+++ src/app/layouts/horizontaltopbar/menu.ts
@@ -31,24 +31,24 @@ export const MENU: MenuItem[] = [
     icon: 'block',
     link: '/blocks',
   },
-  {
-      id: 6,
-      label: 'MENUITEMS.TOKEN.TEXT',
-      icon: 'coin',
-      link: '/tokens',
-      subItems: [
-          {
-              id: 1,
-              label: 'CW-20 Top Tokens',
-              link: '/tokens',
-          },
-          {
-              id: 2,
-              label: 'CW-721 Top Tokens',
-              link: '/tokens/tokens-nft',
-          }
-      ]
-  },
+  // {
+  //     id: 6,
+  //     label: 'MENUITEMS.TOKEN.TEXT',
+  //     icon: 'coin',
+  //     link: '/tokens',
+  //     subItems: [
+  //         {
+  //             id: 1,
+  //             label: 'CW-20 Top Tokens',
+  //             link: '/tokens',
+  //         },
+  //         {
+  //             id: 2,
+  //             label: 'CW-721 Top Tokens',
+  //             link: '/tokens/tokens-nft',
+  //         }
+  //     ]
+  // },
   {
     id: 6,
     label: 'MENUITEMS.CONTRACT.TEXT',
