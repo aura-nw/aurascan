b6b6e635ab72bf759a4fad680b7cbe624bebab53
nttnguyen136 <nttnguyen136@gmail.com>
2022-08-29 20:20:53 +0700

    update paginator

--- src/app/pages/account/account-detail/account-detail.module.ts
+++ src/app/pages/account/account-detail/account-detail.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule, DecimalPipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { SimplebarAngularModule } from 'simplebar-angular';
@@ -8,9 +9,9 @@ import { MaterialModule } from '../../../app.module';
 import { CommonPipeModule } from '../../../core/pipes/common-pipe.module';
 import { AccountService } from '../../../core/services/account.service';
 import { TransactionService } from '../../../core/services/transaction.service';
+import { PaginatorModule } from '../../../shared/components/paginator/paginator.module';
 import { QrModule } from '../../../shared/components/qr/qr.module';
 import { TableNoDataModule } from '../../../shared/components/table-no-data/table-no-data.module';
-import { PaginatorModule } from '../../../shared/components/paginator/paginator.module';
 import { SharedModule } from '../../../shared/shared.module';
 import { AccountDetailRoutingModule } from './account-detail-routing.module';
 import { AccountDetailTableModule } from './account-detail-table/account-detail-table.module';
