9283db23e6be5c77db154439b6e28363091c0bdc
mtam <mtamkg93@gmail.com>
2022-09-26 10:40:01 +0700

    [TamTM6] update layout soud bound

--- src/app/pages/validators/milestone/milestone.component.ts
+++ src/app/pages/validators/milestone/milestone.component.ts
@@ -1,38 +1,74 @@
 import { Component, OnInit } from '@angular/core';
-import {VALIDATOR_AVATAR_DF} from "src/app/core/constants/common.constant";
 
 @Component({
   selector: 'app-milestone',
   templateUrl: './milestone.component.html',
-  styleUrls: ['./milestone.component.scss']
+  styleUrls: ['./milestone.component.scss'],
 })
 export class MilestoneComponent implements OnInit {
   mileStoneData = [
     {
       imgUrl: '',
-      title: 'Euphoria Companion'
+      title: 'Euphoria Companion',
     },
     {
       imgUrl: '',
-      title: '1 year Companion'
+      title: '1 year Companion',
     },
     {
       imgUrl: '',
-      title: '10 millons block commit'
+      title: '10 millions block commit',
     },
     {
       imgUrl: '',
-      title: '3 year Companion'
+      title: '3 year Companion',
     },
     {
       imgUrl: '',
-      title: '5 year Companion'
-    }
-  ]
-  VALIDATOR_AVATAR_DF;
-  constructor() { }
+      title: '5 year Companion',
+    },
+    {
+      imgUrl: '',
+      title: '5 year Companion',
+    },
+    {
+      imgUrl: '',
+      title: '5 year Companion',
+    },
+    {
+      imgUrl: '',
+      title: '5 year Companion',
+    },
+    {
+      imgUrl: '',
+      title: '5 year Companion',
+    },
+    {
+      imgUrl: '',
+      title: '5 year Companion',
+    },
+    {
+      imgUrl: '',
+      title: '5 year Companion',
+    },
+    {
+      imgUrl: '',
+      title: '5 year Companion',
+    },
+    {
+      imgUrl: '',
+      title: '5 year Companion',
+    },
+    {
+      imgUrl: '',
+      title: '5 year Companion',
+    },
+    {
+      imgUrl: '',
+      title: '5 year Companion',
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
