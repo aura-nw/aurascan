cfc05e85e529af159fe40f1963b0061991f1e4b8
letrieuthien <ltthien001@gmail.com>
2022-11-02 15:27:29 +0700

    update chart

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,20 +1,21 @@
-import { DecimalPipe } from '@angular/common';
-import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { Subscription, timer } from 'rxjs';
-import { EnvironmentService } from 'src/app/core/data-services/environment.service';
-import { getInfo } from 'src/app/core/utils/common/info-common';
-import { TYPE_TRANSACTION } from '../../../app/core/constants/transaction.constant';
-import { TRANSACTION_TYPE_ENUM } from '../../../app/core/constants/transaction.enum';
-import { TableTemplate } from '../../../app/core/models/common.model';
-import { BlockService } from '../../../app/core/services/block.service';
-import { CommonService } from '../../../app/core/services/common.service';
-import { TransactionService } from '../../../app/core/services/transaction.service';
-import { CHART_RANGE, PAGE_EVENT } from '../../core/constants/common.constant';
-import { balanceOf } from '../../core/utils/common/parsing';
-import { convertDataBlock, convertDataTransaction, Globals } from '../../global/global';
-import { ChartOptions, DASHBOARD_CHART_OPTIONS } from './dashboard-chart-options';
-import { createChart } from 'lightweight-charts';
+import {DatePipe, DecimalPipe} from '@angular/common';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {Subscription, timer} from 'rxjs';
+import {EnvironmentService} from 'src/app/core/data-services/environment.service';
+import {getInfo} from 'src/app/core/utils/common/info-common';
+import {TYPE_TRANSACTION} from '../../../app/core/constants/transaction.constant';
+import {TRANSACTION_TYPE_ENUM} from '../../../app/core/constants/transaction.enum';
+import {TableTemplate} from '../../../app/core/models/common.model';
+import {BlockService} from '../../../app/core/services/block.service';
+import {CommonService} from '../../../app/core/services/common.service';
+import {TransactionService} from '../../../app/core/services/transaction.service';
+import {CHART_RANGE, PAGE_EVENT} from '../../core/constants/common.constant';
+import {balanceOf} from '../../core/utils/common/parsing';
+import {convertDataBlock, convertDataTransaction, Globals} from '../../global/global';
+import {ChartOptions, DASHBOARD_CHART_OPTIONS} from './dashboard-chart-options';
+import {createChart} from 'lightweight-charts';
+import ExcelExport from 'export-xlsx';
 
 @Component({
   selector: 'app-dashboard',
@@ -30,19 +31,19 @@ export class DashboardComponent implements OnInit {
   public chartOptions: Partial<ChartOptions> = DASHBOARD_CHART_OPTIONS;
 
   templatesBlock: Array<TableTemplate> = [
-    { matColumnDef: 'height', headerCellDef: 'Height' },
-    { matColumnDef: 'proposer', headerCellDef: 'Proposer' },
-    { matColumnDef: 'num_txs', headerCellDef: 'Txs' },
-    { matColumnDef: 'timestamp', headerCellDef: 'Time' },
+    {matColumnDef: 'height', headerCellDef: 'Height'},
+    {matColumnDef: 'proposer', headerCellDef: 'Proposer'},
+    {matColumnDef: 'num_txs', headerCellDef: 'Txs'},
+    {matColumnDef: 'timestamp', headerCellDef: 'Time'},
   ];
   displayedColumnsBlock: string[] = this.templatesBlock.map((dta) => dta.matColumnDef);
   dataSourceBlock: MatTableDataSource<any>;
 
   templatesTx: Array<TableTemplate> = [
-    { matColumnDef: 'tx_hash', headerCellDef: 'Tx Hash' },
-    { matColumnDef: 'height', headerCellDef: 'Height' },
-    { matColumnDef: 'type', headerCellDef: 'Type' },
-    { matColumnDef: 'timestamp', headerCellDef: 'Time' },
+    {matColumnDef: 'tx_hash', headerCellDef: 'Tx Hash'},
+    {matColumnDef: 'height', headerCellDef: 'Height'},
+    {matColumnDef: 'type', headerCellDef: 'Type'},
+    {matColumnDef: 'timestamp', headerCellDef: 'Time'},
   ];
   displayedColumnsTx: string[] = this.templatesTx.map((dta) => dta.matColumnDef);
   dataSourceTx: MatTableDataSource<any> = new MatTableDataSource();
@@ -54,6 +55,38 @@ export class DashboardComponent implements OnInit {
   denom = this.environmentService.configValue.chain_info.currencies[0].coinDenom;
   coinInfo = this.environmentService.configValue.chain_info.currencies[0];
 
+  chart = null;
+  areaSeries = null;
+  chartData;
+  chartDataExp = [];
+
+  SETTINGS_FOR_EXPORT = {
+    // Table settings
+    fileName: 'Transactions',
+    workSheets: [
+      {
+        sheetName: 'Transactions',
+        startingRowNumber: 2,
+        gapBetweenTwoTables: 2,
+        tableSettings: {
+          table1: {
+            tableTitle: "Transactions value by date",
+            headerDefinition: [
+              {
+                name: 'Date',
+                key: 'date',
+              },
+              {
+                name: 'Transactions',
+                key: 'transactions',
+              }
+            ],
+          }
+        }
+      },
+    ],
+  };
+
   constructor(
     public commonService: CommonService,
     private blockService: BlockService,
@@ -62,29 +95,72 @@ export class DashboardComponent implements OnInit {
     private numberPipe: DecimalPipe,
     private environmentService: EnvironmentService,
     private cdr: ChangeDetectorRef,
-  ) {}
+    public datepipe: DatePipe
+  ) {
+  }
 
   ngOnInit(): void {
     this.getInfoData();
     const halftime = 60000;
     this.timerUnSub = timer(halftime, halftime).subscribe(() => this.getInfoData());
+    // somewhere in your code
+    this.chart = createChart(document.getElementById('chart'),
+      {
+        height: 400,
+        crosshair: {
+          horzLine: {
+            visible: false
+          }
+        },
+        layout: {
+          backgroundColor: '#24262e',
+          textColor: '#868a97',
+        },
+        grid: {
+          vertLines: {visible: false},
+          horzLines: {visible: false}
+        },
+        leftPriceScale: {
+          visible: true
+        },
+        rightPriceScale: {
+          visible: false
+        }
+      }
+    );
+    this.areaSeries = this.chart.addAreaSeries();
+    this.areaSeries.applyOptions({
+      lineColor: '#5EE6D0',
+      topColor: 'rgba(136,198,203,0.12)',
+      bottomColor: 'rgba(119, 182, 188, 0.01)',
+    })
   }
 
   drawChart(data, dateTime) {
-    // somewhere in your code
-    let chart = null;
-    chart = createChart(document.getElementById('chart'), { width: 1300, height: 400 });
-    const areaSeries = chart.addAreaSeries();
-    console.log(data);
-
+    this.chartData = null;
+    this.chartDataExp = [];
     let arr = [];
     data.forEach((element, index) => {
-      var temp = { value: element, time: dateTime[index] };
+      var temp = {value: element, time: dateTime[index]};
       arr.push(temp);
     });
+    this.chartData = arr;
 
-    areaSeries.setData(arr);
-    chart.timeScale().fitContent();
+    // setup data for export
+    arr.forEach(data => {
+      const dateF =this.datepipe.transform(data.time, 'dd-MM-yyyy');
+      this.chartDataExp.push({
+        date: dateF,
+        transactions: +data.value
+      })
+    })
+    this.chartDataExp = [
+      {
+        table1: this.chartDataExp
+      }
+    ];
+    this.areaSeries.setData(arr);
+    this.chart.timeScale().fitContent();
   }
 
   //get all data for dashboard
@@ -108,7 +184,7 @@ export class DashboardComponent implements OnInit {
 
   getListBlock(): void {
     this.blockService.blocksIndexer(this.PAGE_SIZE).subscribe((res) => {
-      const { code, data } = res;
+      const {code, data} = res;
       if (code === 200) {
         const blocks = convertDataBlock(data);
         this.dataSourceBlock = new MatTableDataSource(blocks);
@@ -119,7 +195,7 @@ export class DashboardComponent implements OnInit {
   getListTransaction(): void {
     this.transactionService.txsIndexer(this.PAGE_SIZE, 0).subscribe((res) => {
       this.dataSourceTx.data = [];
-      const { code, data } = res;
+      const {code, data} = res;
       if (code === 200) {
         const txs = convertDataTransaction(data, this.coinInfo);
 
@@ -140,27 +216,6 @@ export class DashboardComponent implements OnInit {
       this.getInfoCommon();
       const data1 = res.data.map((i) => i.total);
       let categories = res.data.map((i) => i.timestamp);
-
-      this.chartOptions.series = [
-        {
-          name: 'transactions',
-          type: 'line',
-          data: data1,
-          color: '#5EE6D0',
-        },
-      ];
-
-      this.chartOptions.xAxis = {
-        type: 'datetime',
-        categories: categories,
-        labels: {
-          datetimeUTC: false,
-        },
-        axisBorder: {
-          show: true,
-          color: '#FFA741',
-        },
-      };
 
       this.drawChart(data1, categories);
     });
@@ -189,5 +244,10 @@ export class DashboardComponent implements OnInit {
         `<span class=text--primary> ${this.denom} </span>`
       );
     }
+  }
+
+  chartDataExport() {
+    const excelExport = new ExcelExport();
+    excelExport.downloadExcel(this.SETTINGS_FOR_EXPORT, this.chartDataExp);
   }
 }
