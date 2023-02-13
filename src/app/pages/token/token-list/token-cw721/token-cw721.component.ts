34c582e62391a876956f22f97ffbe5a8145ac2e8
nhphuc2411 <phucnguyen241193@gmail.com>
2023-02-13 11:52:26 +0700

    enhance display search result in list view CW20 token and CW721 token

--- src/app/pages/token/token-list/token-cw721/token-cw721.component.ts
+++ src/app/pages/token/token-list/token-cw721/token-cw721.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { ActivatedRoute } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { PAGE_EVENT } from 'src/app/core/constants/common.constant';
 import { MAX_LENGTH_SEARCH_TOKEN } from 'src/app/core/constants/token.constant';
@@ -16,7 +15,6 @@ import { Globals } from '../../../../global/global';
   templateUrl: './token-cw721.component.html',
   styleUrls: ['./token-cw721.component.scss'],
 })
-
 export class TokenCw721Component implements OnInit {
   textSearch = '';
   filterSearchData = [];
@@ -35,11 +33,9 @@ export class TokenCw721Component implements OnInit {
     pageIndex: PAGE_EVENT.PAGE_INDEX,
   };
   dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
-  dataSourceBk: MatTableDataSource<any> = new MatTableDataSource<any>([]);
   maxLengthSearch = MAX_LENGTH_SEARCH_TOKEN;
   sortedData: any;
   sort: MatSort;
-  enterSearch = '';
   typeSortBy = {
     transfers24h: 'transfers_24h',
     transfers3d: 'transfers_3d',
@@ -54,16 +50,11 @@ export class TokenCw721Component implements OnInit {
   constructor(
     public translate: TranslateService,
     public global: Globals,
-    private route: ActivatedRoute,
     public tokenService: TokenService,
     private environmentService: EnvironmentService,
   ) {}
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe((params) => {
-      this.enterSearch = params?.a || '';
-      this.textSearch = this.enterSearch;
-    });
     this.getTokenData();
   }
 
@@ -71,7 +62,7 @@ export class TokenCw721Component implements OnInit {
     const payload = {
       limit: this.pageData.pageSize,
       offset: this.pageData.pageIndex * this.pageData.pageSize,
-      keyword: this.enterSearch,
+      keyword: this.textSearch,
       sort_column: this.sortBy,
       sort_order: this.sortOrder,
     };
@@ -87,31 +78,9 @@ export class TokenCw721Component implements OnInit {
         });
 
         this.dataSource = new MatTableDataSource<any>(res.data);
-        this.dataSourceBk = this.dataSource;
         this.pageData.length = res.meta.count;
       }
     });
-  }
-
-  searchToken(): void {
-    if (this.textSearch?.length > 0) {
-      let payload = {
-        limit: 0,
-        offset: 0,
-        keyword: this.textSearch,
-      };
-
-      this.tokenService.getListCW721Token(payload).subscribe((res: ResponseDto) => {
-        if (res?.data?.length > 0) {
-          this.dataSearch = res.data;
-        }
-
-        let keyWord = this.textSearch.toLowerCase();
-        this.filterSearchData = this.dataSearch?.filter(
-          (data) => data.name.toLowerCase().includes(keyWord) || data.contract_address.toLowerCase().includes(keyWord),
-        );
-      });
-    }
   }
 
   paginatorEmit(event): void {
@@ -123,19 +92,9 @@ export class TokenCw721Component implements OnInit {
     this.getTokenData();
   }
 
-  setPageList(): void {
-    if (this.filterSearchData?.length > 0) {
-      window.location.href = `/tokens/tokens-nft?a=${this.textSearch}`;
-    }
-  }
-
   resetSearch() {
-    if (this.enterSearch) {
-      window.location.href = `/tokens/tokens-nft`;
-    } else {
-      this.textSearch = '';
-      this.enterSearch = '';
-    }
+    this.textSearch = '';
+    this.getTokenData();
   }
 
   sortData(sort: Sort) {
