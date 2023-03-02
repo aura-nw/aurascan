eb108e18e73d1aba3356b8cdbab4410d2afaa7f5
mtam <mtamkg93@gmail.com>
2023-03-02 15:59:26 +0700

    [TamTM6] update link codeid, get data tab verify code

--- src/app/pages/code-ids/code-id-detail/verify-code-id/verify-code-id.component.ts
+++ src/app/pages/code-ids/code-id-detail/verify-code-id/verify-code-id.component.ts
@@ -9,25 +9,15 @@ import { ContractService } from 'src/app/core/services/contract.service';
   styleUrls: ['./verify-code-id.component.scss'],
 })
 export class VerifyCodeIdComponent implements OnInit {
-  @Input() codeIdDetail: any;
+  @Input() codeId: any;
   loading = true;
+  codeIdDetail: any;
   contractStatus: any = ContractVerifyType.Unverified;
   contractVerifyType = ContractVerifyType;
   constructor(private contractService: ContractService, private router: Router) {}
 
   ngOnInit(): void {
-    this.checkStatusVerify();
-  }
-
-  checkStatusVerify() {
-    this.contractService.checkVerified(this.codeIdDetail.code_id).subscribe((res) => {
-      if (res.data) {
-        this.contractStatus = res.data.status;
-        this.loading = false;
-        if (this.contractStatus === ContractVerifyType.Verified) {
-        }
-      }
-    });
+    this.getCodeIdDetail();
   }
 
   copyData(text: string): void {
@@ -44,9 +34,17 @@ export class VerifyCodeIdComponent implements OnInit {
     }, 800);
   }
 
-  navigateToVerify(codeId: string) {
+  navigateToVerify() {
     let linkVerify = this.router.url.includes('?tab=1') ? this.router.url : this.router.url + '?tab=1';
     sessionStorage.setItem('codeIdPrePage', linkVerify);
-    this.router.navigate(['/contracts/verify', this.codeIdDetail.code_id]);
+    this.router.navigate(['/contracts/verify', this.codeId]);
+  }
+
+  getCodeIdDetail() {
+    this.contractService.getCodeIDDetail(this.codeId).subscribe((res) => {
+      this.codeIdDetail = res.data;
+      this.contractStatus = this.codeIdDetail?.contract_verification;
+      this.loading = false;
+    });
   }
 }
