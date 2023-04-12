77000d9dd95ee9fb60f2a19b7a30e2978fec2f0a
mtam <mtamkg93@gmail.com>
2023-04-12 09:42:08 +0700

    [TamTM6] fix ui validator

--- src/app/pages/validators/validators.component.ts
+++ src/app/pages/validators/validators.component.ts
@@ -34,7 +34,7 @@ import { Globals } from '../../../app/global/global';
 export class ValidatorsComponent implements OnInit, OnDestroy {
   @ViewChild(MatSort) sort: MatSort;
   templates: Array<TableTemplate> = [
-    { matColumnDef: 'rank', headerCellDef: 'Rank', desktopOnly: true },
+    // { matColumnDef: 'rank', headerCellDef: 'Rank', desktopOnly: true },
     { matColumnDef: 'title', headerCellDef: 'Validator', desktopOnly: true },
     { matColumnDef: 'power', headerCellDef: 'Voting Power' },
     { matColumnDef: 'commission', headerCellDef: 'Commission' },
@@ -118,13 +118,11 @@ export class ValidatorsComponent implements OnInit, OnDestroy {
     public commonService: CommonService,
     private walletService: WalletService,
     private toastr: NgxToastrService,
-    private transactionService: TransactionService,
     private mappingErrorService: MappingErrorService,
     private router: Router,
     private layout: BreakpointObserver,
     private scroll: ViewportScroller,
-    private environmentService: EnvironmentService,
-    private global: Globals,
+    private environmentService: EnvironmentService
   ) {}
 
   async ngOnInit() {
@@ -174,6 +172,7 @@ export class ValidatorsComponent implements OnInit, OnDestroy {
           val.participation = val.vote_count;
           val.power = val.power / NUMBER_CONVERT;
           val.up_time = Number(val.up_time.replace('%', ''));
+          val.width_chart = val.up_time / 100;
         });
 
         let dataFilter = res.data.filter((event) =>
