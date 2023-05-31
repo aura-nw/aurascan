12b6e5c1c041bf8ab110f7a6ababbd774ae0f45f
nhphuc2411 <phucnguyen241193@gmail.com>
2023-05-31 11:38:34 +0700

    implement community pool

--- src/app/pages/community-pool/community-pool.component.ts
+++ src/app/pages/community-pool/community-pool.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
-import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { TranslateService } from '@ngx-translate/core';
 import * as _ from 'lodash';
 import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 import { PAGE_EVENT } from 'src/app/core/constants/common.constant';
 import { MAX_LENGTH_SEARCH_TOKEN } from 'src/app/core/constants/token.constant';
 import { EnvironmentService } from 'src/app/core/data-services/environment.service';
@@ -18,7 +18,7 @@ import { PaginatorComponent } from 'src/app/shared/components/paginator/paginato
   templateUrl: './community-pool.component.html',
   styleUrls: ['./community-pool.component.scss'],
 })
-export class CommunityPoolComponent implements OnInit {
+export class CommunityPoolComponent implements OnInit, OnDestroy {
   @ViewChild(PaginatorComponent) pageChange: PaginatorComponent;
   textSearch = '';
   templates: Array<TableTemplate> = [
@@ -28,7 +28,6 @@ export class CommunityPoolComponent implements OnInit {
     { matColumnDef: 'amount', headerCellDef: 'amount' },
   ];
   displayedColumns: string[] = this.templates.map((dta) => dta.matColumnDef);
-
   pageData: PageEvent = {
     length: PAGE_EVENT.LENGTH,
     pageSize: 20,
@@ -36,15 +35,13 @@ export class CommunityPoolComponent implements OnInit {
   };
 
   dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
-  sortedData: any;
-  sort: MatSort;
   filterSearchData = [];
   maxLengthSearch = MAX_LENGTH_SEARCH_TOKEN;
   denom = this.environmentService.configValue.chain_info.currencies[0].coinDenom;
   image_s3 = this.environmentService.configValue.image_s3;
   defaultLogoToken = this.image_s3 + 'images/icons/token-logo.png';
   listCoin = this.environmentService.configValue.coins;
-
+  listAssetLcd = [];
   searchSubject = new Subject();
   destroy$ = new Subject();
 
@@ -55,125 +52,72 @@ export class CommunityPoolComponent implements OnInit {
     private environmentService: EnvironmentService,
   ) {}
 
-  // ngOnDestroy(): void {
-  //   // throw new Error('Method not implemented.');
-  //   this.destroy$.next();
-  //   this.destroy$.complete();
-  // }
+  ngOnDestroy(): void {
+    // throw new Error('Method not implemented.');
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   ngOnInit(): void {
-    this.getListToken();
-
-    // this.searchSubject
-    //   .asObservable()
-    //   .pipe(debounceTime(500), distinctUntilChanged(), takeUntil(this.destroy$))
-    //   .subscribe(() => {
-    //     if (this.pageData.pageIndex === PAGE_EVENT.PAGE_INDEX) {
-    //       this.getListToken();
-    //     } else {
-    //       this.pageChange.selectPage(0);
-    //     }
-    //   });
+    this.getListAsset();
+    this.searchSubject
+      .asObservable()
+      .pipe(debounceTime(500), distinctUntilChanged(), takeUntil(this.destroy$))
+      .subscribe(() => {
+        if (this.pageData.pageIndex === PAGE_EVENT.PAGE_INDEX) {
+          this.getListAsset();
+        } else {
+          this.pageChange.selectPage(0);
+        }
+      });
   }
 
   onKeyUp() {
-    // this.searchSubject.next(this.textSearch);
+    this.searchSubject.next(this.textSearch);
   }
 
-  async getListToken() {
-    const res = await this.tokenService.getListAssetCommunityPool();
-    let listAssetLcd = _.get(res, 'data.pool');
+  async getListAsset() {
+    let auraAsset;
+    if (this.textSearch) {
+      this.filterSearchData = this.listAssetLcd;
+      this.filterSearchData = this.filterSearchData.filter(
+        (k) => k.name.toLowerCase().includes(this.textSearch) === true,
+      );
+      this.dataSource = new MatTableDataSource<any>(this.filterSearchData);
+      this.pageData.length = this.filterSearchData.length;
+    } else {
+      const res = await this.tokenService.getListAssetCommunityPool();
+      this.listAssetLcd = _.get(res, 'data.pool');
 
-    listAssetLcd.forEach((element) => {
-      let test = this.listCoin.find((i) => i.denom === element.denom);
-      console.log(test);
-      if (test) {
-        element.decimal = test.decimal;
-        element.symbol = test.display;
-        element.logo = test.logo;
-        element.name = test.name;
-      } else {
-        element.decimal = 6;
-        element.symbol = '';
-        element.logo = '';
-        element.name = '';
-      }
-    });
-    this.dataSource = new MatTableDataSource<any>(listAssetLcd)
-    this.pageData.length = listAssetLcd.length;
-
-    // this.tokenService.getListToken(payload).subscribe((res: ResponseDto) => {
-    //   res.data.forEach((data) => {
-    //     Object.assign(data, {
-    //       ...data,
-    //       circulating_market_cap: +data.circulating_market_cap || 0,
-    //       onChainMarketCap: +data.circulating_market_cap || 0,
-    //       volume: +data.volume_24h,
-    //       price: +data.price,
-    //       isValueUp: data.price_change_percentage_24h < 0 ? false : true,
-    //       change: Number(data.price_change_percentage_24h.toString()),
-    //       isHolderUp: data.holders_change_percentage_24h < 0 ? false : true,
-    //       holders: +data.holders,
-    //       holderChange: Number(data.holders_change_percentage_24h.toString()),
-    //     });
-    //   });
-
-    //   this.dataSource = new MatTableDataSource<any>(res.data);
-    //   this.pageData.length = res.meta.count;
-    // });
+      this.listAssetLcd.forEach((element) => {
+        let test = this.listCoin.find((i) => i.denom === element.denom);
+        if (test) {
+          element.decimal = test.decimal;
+          element.symbol = test.display;
+          element.logo = test.logo;
+          element.name = test.name;
+        } else {
+          element.decimal = 6;
+          element.symbol = '';
+          element.logo = '';
+          element.name = 'Aura';
+          auraAsset = element;
+        }
+      });
+      this.listAssetLcd = this.listAssetLcd.filter((k) => k.symbol !== '');
+      this.listAssetLcd = this.listAssetLcd.sort((a, b) => {
+        return this.compare(a.amount, b.amount, false);
+      });
+      this.listAssetLcd.unshift(auraAsset);
+      this.filterSearchData = this.listAssetLcd;
+      this.dataSource = new MatTableDataSource<any>(this.listAssetLcd);
+      this.dataSource = new MatTableDataSource<any>(this.listAssetLcd);
+      this.pageData.length = this.listAssetLcd.length;
+    }
   }
 
   paginatorEmit(event): void {
     this.dataSource.paginator = event;
-  }
-
-  sortData(sort: Sort) {
-    this.dataSource.data.forEach((data) => {
-      data.circulating_market_cap = +data.circulating_market_cap;
-      data.volume = +data.volume_24h;
-      data.price = +data.price;
-      data.holders = +data.holders;
-    });
-
-    let data = this.dataSource.data.slice();
-    if (!sort.active || sort.direction === '') {
-      this.sortedData = data;
-      return;
-    }
-
-    const isAsc = sort.direction === 'asc';
-    this.sortedData = data.sort((a, b) => {
-      switch (sort.active) {
-        case 'price':
-          return this.compare(a.price, b.price, isAsc);
-        case 'volume':
-          return this.compare(a.volume, b.volume, isAsc);
-        case 'circulating_market_cap':
-          return this.compare(a.circulating_market_cap, b.circulating_market_cap, isAsc);
-        case 'onChainMarketCap':
-          return this.compare(a.onChainMarketCap, b.onChainMarketCap, isAsc);
-        default:
-          return 0;
-      }
-    });
-
-    if (sort.active === 'change') {
-      let lstUp = this.sortedData
-        .filter((data) => data.isValueUp)
-        ?.sort((a, b) => this.compare(a.change, b.change, isAsc));
-      let lstDown = this.sortedData
-        .filter((data) => !data.isValueUp)
-        .sort((a, b) => this.compare(a.change, b.change, !isAsc));
-      this.sortedData = isAsc ? lstDown.concat(lstUp) : lstUp.concat(lstDown);
-    }
-
-    let dataFilter = this.sortedData;
-    this.pageData = {
-      length: this.pageData.length,
-      pageSize: this.pageData.pageSize,
-      pageIndex: this.pageData.pageIndex,
-    };
-    this.dataSource = new MatTableDataSource<any>(dataFilter);
   }
 
   compare(a: number | string, b: number | string, isAsc: boolean) {
@@ -182,11 +126,11 @@ export class CommunityPoolComponent implements OnInit {
 
   resetSearch() {
     this.textSearch = '';
-    // this.onKeyUp();
+    this.onKeyUp();
   }
 
   pageEvent(e: PageEvent): void {
     this.pageData.pageIndex = e.pageIndex;
-    this.getListToken();
+    this.getListAsset();
   }
 }
