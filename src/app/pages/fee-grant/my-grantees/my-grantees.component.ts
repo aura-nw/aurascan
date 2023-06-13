a2d01ff04cbc516da2725e141c7b669a50fec101
mtam <mtamkg93@gmail.com>
2023-06-13 16:08:50 +0700

    [TamTM6] update feegrant v2

--- src/app/pages/fee-grant/my-grantees/my-grantees.component.ts
+++ src/app/pages/fee-grant/my-grantees/my-grantees.component.ts
@@ -4,7 +4,7 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { TranslateService } from '@ngx-translate/core';
 import * as _ from 'lodash';
-import { Subject, Subscription } from 'rxjs';
+import { Subject } from 'rxjs';
 import { PAGE_EVENT } from 'src/app/core/constants/common.constant';
 import { MAX_LENGTH_SEARCH_TOKEN } from 'src/app/core/constants/token.constant';
 import { TYPE_TRANSACTION } from 'src/app/core/constants/transaction.constant';
@@ -59,9 +59,8 @@ export class MyGranteesComponent implements OnInit {
   nextKey = null;
   currentKey = null;
   currentAddress = null;
-  filterSearch = {};
   destroyed$ = new Subject();
-  timerUnSub: Subscription;
+  timerGetFeeGrant: any;
 
   denom = this.environmentService.configValue.chain_info.currencies[0].coinDenom;
   chainInfo = this.environmentService.configValue.chain_info;
@@ -88,6 +87,10 @@ export class MyGranteesComponent implements OnInit {
         this.currentAddress = null;
       }
     });
+
+    this.timerGetFeeGrant = setInterval(() => {
+      this.getListGrant();
+    }, 30000);
   }
 
   /**
@@ -96,14 +99,10 @@ export class MyGranteesComponent implements OnInit {
   ngOnDestroy(): void {
     this.destroyed$.next();
     this.destroyed$.complete();
-
-    if (this.timerUnSub) {
-      this.timerUnSub.unsubscribe();
-    }
+    clearInterval(this.timerGetFeeGrant);
   }
 
   getGranteesData() {
-    this.loading = true;
     if (this.isActive) {
       this.templates = this.templatesActive;
       this.displayedColumns = this.templatesActive.map((dta) => dta.matColumnDef);
@@ -115,62 +114,57 @@ export class MyGranteesComponent implements OnInit {
   }
 
   getListGrant() {
-    const halftime = 30000;
-    this.filterSearch['isGranter'] = false;
-    this.filterSearch['isActive'] = this.isActive;
+    const payload = {
+      limit: 100,
+      granter: this.currentAddress,
+      isActive: this.isActive,
+      isGranter: false,
+    };
 
-    this.feeGrantService.getListFeeGrants(this.filterSearch, this.currentAddress, this.nextKey, false).subscribe(
+    this.feeGrantService.getListFeeGrantsV2(payload, this.textSearch).subscribe(
       (res) => {
-        const { code, data } = res;
-        if (code === 200) {
-          this.nextKey = res.data.nextKey || null;
-          data.grants.forEach((element) => {
-            element.type = _.find(TYPE_TRANSACTION, { label: element.type })?.value;
-            element.limit = element?.spend_limit?.amount || '0';
-            element.spendable = element?.amount?.amount || '0';
-            element.reason = element?.status;
-            if (element.reason === 'Available' && element?.expired) {
+        res.feegrant?.forEach((element) => {
+          element.type = _.find(TYPE_TRANSACTION, { label: element.type })?.value;
+          element.spendable = element?.spend_limit || '0';
+          element.limit = element.feegrant_histories[0]?.amount || '0';
+          element.reason = element?.status;
+          element.tx_hash = element?.transaction?.hash;
+          element.timestamp = element?.transaction?.timestamp;
+          element.origin_revoke_txhash = element?.revoke_tx?.hash;
+          if (element?.expiration) {
+            const timeCompare = new Date(element?.expiration).getTime();
+            if (element.status === 'Available' && timeCompare < Date.now()) {
               element.reason = 'Expired';
             }
-          });
-
-          if (
-            this.dataSource?.data?.length > 0 &&
-            this.dataSource.data.length !== data.grants.length &&
-            this.pageData.pageIndex != 0
-          ) {
-            this.dataSource.data = [...this.dataSource.data, ...data.grants];
-          } else {
-            this.dataSource.data = [...data.grants];
           }
-          this.pageData.length = this.dataSource.data.length;
-        }
-        this.loading = false;
+        });
 
-        setTimeout(() => {
-          this.getListGrant();
-        }, halftime);
+        if (
+          this.dataSource?.data?.length > 0 &&
+          this.dataSource.data.length !== res.feegrant?.length &&
+          this.pageData.pageIndex != 0
+        ) {
+          this.dataSource.data = [...this.dataSource.data, ...res.feegrant];
+        } else {
+          this.dataSource.data = [...res.feegrant];
+        }
+        this.pageData.length = res.feegrant?.length;
       },
-      (error) => {
-        setTimeout(() => {
-          this.getListGrant();
-        }, halftime);
+      (error) => {},
+      () => {
+        this.loading = false;
       },
     );
   }
 
   searchToken(): void {
     if (this.textSearch && this.textSearch.length > 0) {
-      this.dataSource.data = [];
-      this.filterSearch['textSearch'] = this.textSearch;
       this.getListGrant();
     }
   }
 
   resetFilterSearch() {
     this.textSearch = '';
-    this.filterSearch['textSearch'] = '';
-    this.dataSource.data = [];
     this.getListGrant();
   }
 
@@ -240,5 +234,4 @@ export class MyGranteesComponent implements OnInit {
       }
     });
   }
-
 }
