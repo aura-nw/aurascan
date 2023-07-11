0ec51143a0209af2d9faf38aa62866914f8a56fa
letrieuthien <ltthien001@gmail.com>
2023-07-11 14:37:34 +0700

    update profile layout

--- src/app/pages/profile/profile-routing.module.ts
+++ src/app/pages/profile/profile-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from "./profile.component";
-import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ProfileComponent
-  },
-  {
-    path: 'profile-settings',
-    component: ProfileSettingsComponent
   }
 ];
 
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit {
+
+  currentPage: 'setting' | 'nameTage' | 'watchlist' = "setting";
+
+  constructor(
+    private route: ActivatedRoute,) {
+  }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      if(params?.tab) {
+        this.currentPage = params.tab
+      }
+    });
+  }
+}
