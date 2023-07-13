ab26bb570d3a674e0f53608ec478ddee0c5af0d3
mtam <mtamkg93@gmail.com>
2023-07-13 14:45:14 +0700

    [TamTM6] name tag remove backgound, fix layout contract detail

--- src/app/pages/contracts/contracts-detail/contracts-overview-card/contracts-overview-card.component.ts
+++ src/app/pages/contracts/contracts-detail/contracts-overview-card/contracts-overview-card.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { TYPE_ACCOUNT } from 'src/app/core/constants/account.constant';
 import { EnvironmentService } from 'src/app/core/data-services/environment.service';
 import { Globals } from '../../../../global/global';
-import {ContractService} from "src/app/core/services/contract.service";
+import { ContractService } from 'src/app/core/services/contract.service';
+import { LENGTH_CHARACTER } from 'src/app/core/constants/common.constant';
 
 @Component({
   selector: 'app-contracts-overview-card',
@@ -16,19 +17,21 @@ export class ContractsOverviewCardComponent implements OnInit, OnChanges {
   priceToken = 0;
   selectedToken = '$0.00';
   assetsType = TYPE_ACCOUNT;
+  lengthNormalAddress = LENGTH_CHARACTER.ADDRESS;
+
   denom = this.environmentService.configValue.chain_info.currencies[0].coinDenom;
 
   constructor(
     public global: Globals,
     private environmentService: EnvironmentService,
-    private contractService: ContractService) {}
+    private contractService: ContractService,
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   async ngOnChanges(changes: SimpleChanges) {
     const balanceReq = await this.contractService.getContractBalance(this.contractDetail.address);
-    if(balanceReq?.data) {
+    if (balanceReq?.data) {
       this.contractBalance = balanceReq.data.balances[0] ? balanceReq.data.balances[0] : 0;
       this.contractPrice = this.contractBalance * this.priceToken || 0;
     }
