53f8fa59ab3b9b580fd90297da0b878e7c11263c
FISAURA-PHUONGL\quannn7 <quannguyen2724@gmail.com>
2023-08-02 16:49:13 +0700

    layout for account detail page

---
+++ src/app/pages/account/account-detail/stake/stake.component.ts
@@ -0,0 +1,140 @@
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import * as _ from 'lodash';
+import { PAGE_EVENT } from 'src/app/core/constants/common.constant';
+import { MAX_LENGTH_SEARCH_TOKEN } from 'src/app/core/constants/token.constant';
+import { EnvironmentService } from 'src/app/core/data-services/environment.service';
+import { AccountService } from 'src/app/core/services/account.service';
+import { checkTypeFile } from 'src/app/core/utils/common/info-common';
+import { Globals } from 'src/app/global/global';
+
+@Component({
+  selector: 'app-stake',
+  templateUrl: './stake.component.html',
+  styleUrls: ['./stake.component.scss'],
+})
+export class NftListComponent implements OnChanges {
+  @Input() address: string;
+  @Output() totalValueNft = new EventEmitter<number>();
+  image_s3 = this.environmentService.configValue.image_s3;
+  searchValue = '';
+  loading = true;
+  pageData: PageEvent = {
+    length: PAGE_EVENT.LENGTH,
+    pageSize: 10,
+    pageIndex: 1,
+  };
+  nftFilter = null;
+  nftList = [];
+  maxLengthSearch = MAX_LENGTH_SEARCH_TOKEN;
+  totalValue = 0;
+  textSearch = '';
+  listCollection = [
+    {
+      label: 'All',
+      quantity: 0,
+    },
+  ];
+
+  constructor(
+    private accountService: AccountService,
+    public global: Globals,
+    private environmentService: EnvironmentService,
+  ) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.address) {
+      this.pageEvent(0);
+    }
+  }
+
+  getNftData() {
+    const payload = {
+      owner: this.address,
+      limit: this.pageData.pageSize,
+      keyword: this.textSearch,
+      offset: (this.pageData.pageIndex - 1) * this.pageData.pageSize,
+      name: (this.nftFilter === 'All' ? null : this.nftFilter),
+    };
+
+    this.accountService.getAssetCW721ByOwner(payload).subscribe(
+      (res) => {
+        if (res?.cw721_token?.length > 0) {
+          this.nftList = res?.cw721_token;
+          this.pageData.length = res.cw721_token_aggregate?.aggregate?.count;
+
+          this.nftList.forEach((element) => {
+            element.contract_address = _.get(element, 'cw721_contract.smart_contract.address');
+            element.token_name = _.get(element, 'cw721_contract.name');
+            if (!this.searchValue) {
+              this.totalValue += element.price * +element.balance || 0;
+            }
+          });
+          this.totalValueNft.emit(this.totalValue);
+        } else {
+          this.pageData.length = 0;
+        }
+      },
+      () => {},
+      () => {
+        this.loading = false;
+      },
+    );
+  }
+
+  getListCollection() {
+    const payload = {
+      owner: this.address,
+    };
+
+    this.accountService.getListCollectionByOwner(payload).subscribe(
+      (res) => {
+        if (res?.cw721_contract?.length > 0) {
+          res.cw721_contract.forEach((item) => {
+            this.listCollection.push({
+              label: item.name,
+              quantity: item.cw721_tokens_aggregate.aggregate.count,
+            });
+          });
+          this.listCollection[0].quantity = res?.cw721_token_aggregate?.aggregate?.count;
+        }
+      },
+      () => {},
+      () => {},
+    );
+  }
+
+  resetSearch(): void {
+    this.searchValue = '';
+    this.textSearch = '';
+    this.pageEvent(0);
+  }
+
+  searchTokenNft(): void {
+    this.searchValue = this.searchValue?.trim();
+    this.textSearch = this.searchValue;
+    this.pageEvent(0);
+  }
+
+  pageEvent(pageIndex: number): void {
+    // reset page 1 if pageIndex = 0
+    if (pageIndex === 0) {
+      this.pageData.pageIndex = 1;
+    }
+    this.getNftData();
+    this.getListCollection();
+  }
+
+  handleRouterLink(link): void {
+    window.location.href = link;
+  }
+
+  getTypeFile(nft: any) {
+    let nftType = checkTypeFile(nft);
+    return nftType;
+  }
+
+  encodeData(data) {
+    return encodeURIComponent(data);
+  }
+}
