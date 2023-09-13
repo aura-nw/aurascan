9abb7848b1c58b57372c50cb236a6bb67722deae
mtam <mtamkg93@gmail.com>
2023-09-13 14:24:59 +0700

    [TamTM6] css tooltip, name tag

--- src/app/core/utils/common/validation.ts
+++ src/app/core/utils/common/validation.ts
@@ -1,12 +1,12 @@
+import { LENGTH_CHARACTER } from "../../constants/common.constant";
+
 const REGEX = {
   CONTRACT_ADDRESS: /aura([\w\d]+)/,
 };
 
 export function isContract(adr: string): boolean {
-  if (adr) {
-    const regex = new RegExp(REGEX.CONTRACT_ADDRESS);
-
-    return regex.test(adr);
+  if (adr?.startsWith('aura') && adr?.length === LENGTH_CHARACTER.CONTRACT) {
+    return true;
   }
   return false;
 }
