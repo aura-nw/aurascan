8d9011b1be4e4fb08e19ec7510c129575b708c37
nhphuc2411 <phucnguyen241193@gmail.com>
2023-09-21 15:26:42 +0700

    update UI filter
    (cherry picked from commit e752aacf10dfc1c6085d2b0428cea755ecf8c107)

--- src/app/pages/account/account-detail/account-transaction-table/account-transaction-table.component.ts
+++ src/app/pages/account/account-detail/account-transaction-table/account-transaction-table.component.ts
@@ -316,7 +316,7 @@ export class AccountTransactionTableComponent {
 
     setTimeout(() => {
       this.transactionLoading = false;
-    }, 5000);
+    }, 2000);
   }
 
   getListTypeFilter() {
@@ -528,9 +528,9 @@ export class AccountTransactionTableComponent {
 
   datePickerClickTrigger() {
     const toggle = document.getElementById('datepicker-toggle');
-    if(toggle) {
+    if (toggle) {
       const btn = toggle.children[0];
-      if(btn instanceof HTMLElement) btn.click();
+      if (btn instanceof HTMLElement) btn.click();
     }
   }
 
@@ -539,6 +539,10 @@ export class AccountTransactionTableComponent {
   }
 
   initTnxFilterPanel() {
-    this.listTypeSelectedTemp = this.tnxTypeOrigin.filter(type => this.transactionFilter.type.includes(type.label));
+    if (this.transactionFilter.type) {
+      this.listTypeSelectedTemp = this.tnxTypeOrigin.filter((type) => this.transactionFilter.type.includes(type.label));
+    } else {
+      this.listTypeSelectedTemp = [];
+    }
   }
 }
