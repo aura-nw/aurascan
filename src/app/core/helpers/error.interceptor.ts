e744c4ec9aff9d4b73b48094edacfa7f51500848
mtam <mtamkg93@gmail.com>
2023-09-26 11:43:43 +0700

    [TamTM6] update refresh token

--- src/app/core/helpers/error.interceptor.ts
+++ src/app/core/helpers/error.interceptor.ts
@@ -3,22 +3,49 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((err) => {
         // check status unauthorized, refresh token
-        if (err?.error?.statusCode === 401 && err?.error?.message != 'Incorrect old password.') {
+        console.log(err);
+
+        if (err?.error?.statusCode === 401 && err?.error?.message == 'Unauthorized') {
           const payload = {
             refreshToken: localStorage.getItem('refreshToken').replace(/"/g, ''),
           };
-          this.userService.refreshToken(payload).subscribe((res) => {
-            localStorage.setItem('accessToken', JSON.stringify(res.accessToken));
-            localStorage.setItem('refreshToken', JSON.stringify(res.refreshToken));
+          this.userService.refreshToken(payload).subscribe({
+            next: (res) => {
+              if (res.error?.statusCode === 400) {
+                // redirect to log out
+                localStorage.removeItem('accessToken');
+                localStorage.removeItem('refreshToken');
+                localStorage.removeItem('userEmail');
+                this.router.navigate(['/login']);
+
+                setTimeout(() => {
+                  location.reload();
+                }, 500);
+              }
+
+              localStorage.setItem('accessToken', JSON.stringify(res.accessToken));
+              localStorage.setItem('refreshToken', JSON.stringify(res.refreshToken));
+            },
+            error: () => {
+              localStorage.removeItem('accessToken');
+              localStorage.removeItem('refreshToken');
+              localStorage.removeItem('userEmail');
+              this.router.navigate(['/login']);
+
+              setTimeout(() => {
+                location.reload();
+              }, 500);
+            },
           });
         }
         const error = err.error || err.statusText;
