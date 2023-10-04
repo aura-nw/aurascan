206335b970854ae1853844457a15aec4ad447a4d
mtam <mtamkg93@gmail.com>
2023-10-04 11:18:40 +0700

    [TamTM6] csv part 9

--- src/app/pages/account/account-detail/account-transaction-table/account-transaction-table.component.ts
+++ src/app/pages/account/account-detail/account-transaction-table/account-transaction-table.component.ts
@@ -571,6 +571,7 @@ export class AccountTransactionTableComponent {
 
   pageChangeRecord(event) {
     this.transactionLoading = true;
+    this.nextKey = null;
     this.pageData.pageSize = event;
     this.dataSource = new MatTableDataSource();
     this.getTxsAddress(this.nextKey);
