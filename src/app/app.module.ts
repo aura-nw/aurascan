780a75e6cc3a0f08687c0cfaa51f2924f95f6ff7
nttnguyen136 <nttnguyen136@gmail.com>
2023-10-16 14:28:12 +0700

    change config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { Globals } from './global/global';
 import { LayoutsModule } from './layouts/layouts.module';
 import { MediaExpandModule } from './shared/components/media-expand/media-expand.module';
 import { SchemaViewerModule } from './pages/schema-viewer/schema-viewer.module';
+import { ConfigurationService } from './core/data-services/configuration.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -54,6 +55,12 @@ import { SchemaViewerModule } from './pages/schema-viewer/schema-viewer.module';
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
     DatePipe,
     Globals,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (environmentService: ConfigurationService) => () => environmentService.load(),
+      multi: true,
+      deps: [ConfigurationService],
+    },
     {
       provide: APP_INITIALIZER,
       useFactory: (environmentService: EnvironmentService) => () => environmentService.load(),
