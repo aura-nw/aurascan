7baaa6e05d63ab982817fb6c902662da7e4f2032
nhphuc2411 <phucnguyen241193@gmail.com>
2023-11-21 14:50:55 +0700

    update sort list Token CW20, update display proposal, update text

--- src/app/core/pipes/json.pipe.ts
+++ src/app/core/pipes/json.pipe.ts
@@ -1,13 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
 import { JsonPrettyPrint } from '../utils/common/json-viewer';
 
 @Pipe({
-  name: 'appJson'
+  name: 'appJson',
 })
 export class JsonPipe implements PipeTransform {
-
   transform(value: string, ...args: unknown[]): unknown {
+    if (_.get(args, '[0].checkType')) {
+      return this.transformValue(value);
+    }
     return JsonPrettyPrint(value);
   }
 
+  transformValue(value) {
+    if (typeof value === 'string') {
+      return value;
+    }
+    return JSON.stringify(value);
+  }
 }
