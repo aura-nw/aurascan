3f1033c32cc94f8ffafca9a657e4493b2c9473ee
nttnguyen136 <nttnguyen136@gmail.com>
2023-11-28 10:27:51 +0700

    fix chart

--- src/app/core/constants/common.constant.ts
+++ src/app/core/constants/common.constant.ts
@@ -81,3 +81,4 @@ export const CW721_TRACKING = ['mint', 'burn', 'transfer_nft', 'send_nft'];
 export const MAX_NUMBER_INPUT = 100000000000000;
 export const NUMBER_ONLY_DECIMAL = '1.0-0';
 export const NUMBER_2_DIGIT = '1.2-2';
+export const NUMBER_6_DIGIT = '1.2-6';
