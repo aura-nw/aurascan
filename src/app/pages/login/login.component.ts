1c7011680c9aec5c35d1b1b84f30557ba9bcbfc4
letrieuthien <ltthien001@gmail.com>
2023-11-29 09:29:56 +0700

    detect login wrong password

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import { UntypedFormBuilder, Validators } from '@angular/forms';
 import { MatLegacyDialogConfig as MatDialogConfig } from '@angular/material/legacy-dialog';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -43,6 +43,7 @@ export class LoginComponent implements OnInit {
     private route: Router,
     private environmentService: EnvironmentService,
     private toastr: NgxToastrService,
+    private cdr: ChangeDetectorRef,
     private notificationsService: NotificationsService,
   ) {}
 
@@ -152,6 +153,7 @@ export class LoginComponent implements OnInit {
             }
           },
           error: (err) => {
+            this.cdr.detectChanges();
             const error = err?.error?.error?.details;
             this.addError(error?.message || err.statusText);
             this.errorCode = error?.code;
