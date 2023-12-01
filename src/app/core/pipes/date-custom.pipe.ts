91b5db640c91227c41f09993722a8892bb68d353
letrieuthien <ltthien001@gmail.com>
2023-12-01 08:27:58 +0700

    update dateCustom pipe

--- src/app/core/pipes/date-custom.pipe.ts
+++ src/app/core/pipes/date-custom.pipe.ts
@@ -1,10 +1,40 @@
 import {CommonService} from "src/app/core/services/common.service";
 import { Pipe, PipeTransform } from "@angular/core";
+import {formatTimeInWords, formatWithSchema} from "src/app/core/helpers/date";
+import {DATEFORMAT} from "src/app/core/constants/common.constant";
+import * as moment from "moment";
+import {formatDistanceToNowStrict} from "date-fns";
 
 @Pipe({ name: 'dateCustom' })
 export class DateCustom implements PipeTransform {
   constructor(public commonService: CommonService) {}
   transform(time, isCustom = true) {
-    return this.commonService.getDateValue(time, isCustom);
+    if (time) {
+      try {
+        //get custom function format date if isCustom
+        if (isCustom) {
+          return [
+            formatTimeInWords(new Date(time).getTime()),
+            `(${formatWithSchema(new Date(time).getTime(), DATEFORMAT.DATETIME_UTC)})`,
+          ];
+        } else {
+          if (+moment(time).format('x') - +moment().format('x') > 0) {
+            return [
+              formatWithSchema(new Date(time).getTime(), DATEFORMAT.DATETIME_UTC),
+              formatDistanceToNowStrict(new Date(time).getTime()) + ' remaining',
+            ];
+          } else {
+            return [
+              formatTimeInWords(new Date(time).getTime()),
+              `${formatWithSchema(new Date(time).getTime(), DATEFORMAT.DATETIME_UTC)}`,
+            ];
+          }
+        }
+      } catch (e) {
+        return [time, ''];
+      }
+    } else {
+      return ['-', ''];
+    }
   }
 }
