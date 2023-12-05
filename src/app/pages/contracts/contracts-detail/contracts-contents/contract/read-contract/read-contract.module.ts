7cc4389e13b6d5efbddd468270ac14389b28ae9d
nhphuc2411 <phucnguyen241193@gmail.com>
2023-12-05 13:21:48 +0700

    update translate code id, contract

--- src/app/pages/contracts/contracts-detail/contracts-contents/contract/read-contract/read-contract.module.ts
+++ src/app/pages/contracts/contracts-detail/contracts-contents/contract/read-contract/read-contract.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { CommonPipeModule } from 'src/app/core/pipes/common-pipe.module';
 import { TableNoDataModule } from 'src/app/shared/components/table-no-data/table-no-data.module';
 import { MaterialModule } from '../../../../../../material.module';
@@ -9,7 +10,15 @@ import { ReadContractComponent } from './read-contract.component';
 
 @NgModule({
   declarations: [ReadContractComponent],
-  imports: [CommonModule, CommonPipeModule, TableNoDataModule, RouterModule, FormsModule, MaterialModule],
+  imports: [
+    CommonModule,
+    CommonPipeModule,
+    TableNoDataModule,
+    RouterModule,
+    FormsModule,
+    MaterialModule,
+    TranslateModule,
+  ],
   exports: [ReadContractComponent],
 })
 export class ReadContractModule {}
--- src/app/pages/contracts/contracts-detail/contracts-contents/contract/write-contact/write-contract.module.ts
+++ src/app/pages/contracts/contracts-detail/contracts-contents/contract/write-contact/write-contract.module.ts
@@ -2,15 +2,25 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { CommonPipeModule } from 'src/app/core/pipes/common-pipe.module';
+import { MaterialModule } from 'src/app/material.module';
 import { TableNoDataModule } from 'src/app/shared/components/table-no-data/table-no-data.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { WriteContractComponent } from './write-contract.component';
-import { MaterialModule } from 'src/app/material.module';
 
 @NgModule({
   declarations: [WriteContractComponent],
-  imports: [CommonModule, CommonPipeModule, TableNoDataModule, MaterialModule, RouterModule, FormsModule, SharedModule],
+  imports: [
+    CommonModule,
+    CommonPipeModule,
+    TableNoDataModule,
+    MaterialModule,
+    RouterModule,
+    FormsModule,
+    SharedModule,
+    TranslateModule,
+  ],
   exports: [WriteContractComponent],
 })
 export class WriteContractModule {}
