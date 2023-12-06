22e14df6a9bf7466e94c39ff6e31cdfee8292044
mtam <mtamkg93@gmail.com>
2023-12-06 16:28:27 +0700

    [TamTM6] height transfer asset channel detail

--- src/app/pages/ibc/channel-detail/transfer-assets/transfer-assets.component.ts
+++ src/app/pages/ibc/channel-detail/transfer-assets/transfer-assets.component.ts
@@ -30,11 +30,6 @@ export class TransferAssetsComponent {
     { matColumnDef: 'total_messages', headerCellDef: 'Messages' },
     { matColumnDef: 'amount', headerCellDef: 'Sending amount' },
   ];
-  pageIBCSend: PageEvent = {
-    length: PAGE_EVENT.LENGTH,
-    pageSize: 5,
-    pageIndex: PAGE_EVENT.PAGE_INDEX,
-  };
   dataSourceMobSend: any[];
   displayedColumnsIBC: string[] = this.templatesIBC.map((dta) => dta.matColumnDef);
   isLoadingIBCSend = true;
@@ -49,11 +44,6 @@ export class TransferAssetsComponent {
     { matColumnDef: 'total_messages', headerCellDef: 'Messages' },
     { matColumnDef: 'amount', headerCellDef: 'Receiving amount' },
   ];
-  pageIBCReceive: PageEvent = {
-    length: PAGE_EVENT.LENGTH,
-    pageSize: 5,
-    pageIndex: PAGE_EVENT.PAGE_INDEX,
-  };
   dataSourceMobReceive: any[];
   isLoadingIBCReceive = true;
   textSearchReceive;
@@ -108,7 +98,6 @@ export class TransferAssetsComponent {
           const txs = this.convertTxAssets(res.view_ibc_channel_detail_statistic);
           this.lstSendingRaw = txs;
           this.dataIBCSending.data = [...txs];
-          this.pageIBCSend.length = txs?.length || 0;
         }
       },
       error: (e) => {
@@ -167,7 +156,6 @@ export class TransferAssetsComponent {
           const txs = this.convertTxAssets(res.view_ibc_channel_detail_statistic);
           this.lstSendingReceive = txs;
           this.dataIBCReceiving.data = [...txs];
-          this.pageIBCReceive.length = txs?.length || 0;
         }
       },
       error: (e) => {
