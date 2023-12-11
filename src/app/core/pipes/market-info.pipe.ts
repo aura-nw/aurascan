f9a5fc91c02087bbf43a6f48aadd9d0d837da44b
nttnguyen136 <nttnguyen136@gmail.com>
2023-12-11 13:47:29 +0700

    update

--- src/app/core/pipes/market-info.pipe.ts
+++ src/app/core/pipes/market-info.pipe.ts
@@ -4,47 +4,56 @@ import { TokenService } from 'src/app/core/services/token.service';
 
 @Pipe({ name: 'marketInfo' })
 export class MarketInfoPipe implements PipeTransform {
+  image_s3 = this.env.imageUrl;
+  defaultLogoToken = `${this.image_s3}images/icons/token-logo.png`;
+
   constructor(private env: EnvironmentService, private token: TokenService) {}
 
   transform(value: any, key?: 'logo' | 'name' | 'symbol'): any {
-    if (!value) {
-      return {
-        logo: '',
-        symbol: '',
-        name: '',
-      };
-    }
-    const tokenMarket = this.token.tokensMarket;
+    let marketInfo = {
+      logo: this.defaultLogoToken,
+      symbol: '',
+      name: '',
+    };
 
-    const image_s3 = this.env.imageUrl;
-    const defaultLogoToken = `${image_s3}images/icons/token-logo.png`;
+    if (!value) {
+      return marketInfo;
+    }
+
+    const tokenMarket = this.token.tokensMarket || [];
 
     const { cw20_contract, ibc_denom } = value;
-
-    let logo = cw20_contract?.marketing_info?.logo?.url || defaultLogoToken;
-    let symbol = cw20_contract?.symbol || '';
-    let name = cw20_contract?.name || '';
+    if (cw20_contract) {
+      marketInfo = {
+        logo: cw20_contract?.marketing_info?.logo?.url || this.defaultLogoToken,
+        symbol: cw20_contract?.symbol || '',
+        name: cw20_contract?.name || '',
+      };
+    }
 
     if (cw20_contract?.smart_contract?.address) {
       // cw20 type
       const address = cw20_contract.smart_contract.address;
       const tokenCw20 = tokenMarket.find((t) => t.contract_address === address);
       if (tokenCw20) {
-        logo = tokenCw20.image || logo;
-        name = tokenCw20.name || name;
-        symbol = tokenCw20.symbol || symbol;
+        marketInfo = {
+          logo: tokenCw20.image || marketInfo.logo,
+          symbol: tokenCw20.name || marketInfo.name,
+          name: tokenCw20.symbol || marketInfo.symbol,
+        };
       }
     } else if (ibc_denom) {
       // ibc type
       const tokenIbc = tokenMarket.find((t) => t.denom === ibc_denom);
       if (tokenIbc) {
-        logo = tokenIbc.image || logo;
-        name = tokenIbc.name || name;
-        symbol = tokenIbc.symbol || symbol;
+        marketInfo = {
+          logo: tokenIbc.image || marketInfo.logo,
+          symbol: tokenIbc.name || marketInfo.name,
+          name: tokenIbc.symbol || marketInfo.symbol,
+        };
       }
     }
 
-    const marketInfo = { logo, name, symbol };
     return key ? marketInfo[key] : marketInfo;
   }
 }
