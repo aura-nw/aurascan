187524e8ece15db33c3cd6ffea453b7857e60466
nhphuc2411 <phucnguyen241193@gmail.com>
2023-12-11 08:58:41 +0700

    format code

--- src/app/pages/account/account-detail/account-detail-table/account-detail-table.module.ts
+++ src/app/pages/account/account-detail/account-detail-table/account-detail-table.module.ts
@@ -7,11 +7,11 @@ import { MASK_CONFIG } from 'src/app/app.config';
 import { CommonDirectiveModule } from 'src/app/core/directives/common-directive.module';
 import { MaterialModule } from 'src/app/material.module';
 import { LoadingImageModule } from 'src/app/shared/components/loading-image/loading-image.module';
+import { TooltipCustomizeModule } from 'src/app/shared/components/tooltip-customize/tooltip-customize.module';
 import { CommonPipeModule } from '../../../../core/pipes/common-pipe.module';
 import { PaginatorModule } from '../../../../shared/components/paginator/paginator.module';
 import { TableNoDataModule } from '../../../../shared/components/table-no-data/table-no-data.module';
 import { AccountDetailTableComponent } from './account-detail-table.component';
-import {TooltipCustomizeModule} from "src/app/shared/components/tooltip-customize/tooltip-customize.module";
 
 @NgModule({
   declarations: [AccountDetailTableComponent],
