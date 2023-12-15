31b15848e7db0c7b0688b2e0ec670b4ad17f0ac6
letrieuthien <ltthien001@gmail.com>
2023-12-15 15:06:45 +0700

    fix bug format amount

--- src/app/pages/community-pool/proposal-list/community-pool-proposal.component.ts
+++ src/app/pages/community-pool/proposal-list/community-pool-proposal.component.ts
@@ -1,17 +1,17 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { LegacyPageEvent as PageEvent, MatLegacyPaginator as MatPaginator } from '@angular/material/legacy-paginator';
-import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
-import { TranslateService } from '@ngx-translate/core';
-import { tap } from 'rxjs/operators';
-import { PAGE_EVENT, TIMEOUT_ERROR } from 'src/app/core/constants/common.constant';
-import { PROPOSAL_STATUS, PROPOSAL_TYPE_COMMUNITY_POOL } from 'src/app/core/constants/proposal.constant';
-import { EnvironmentService } from 'src/app/core/data-services/environment.service';
-import { TableTemplate } from 'src/app/core/models/common.model';
-import { CommonService } from 'src/app/core/services/common.service';
-import { ProposalService } from 'src/app/core/services/proposal.service';
-import { shortenAddress } from 'src/app/core/utils/common/shorten';
-import { PaginatorComponent } from 'src/app/shared/components/paginator/paginator.component';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {LegacyPageEvent as PageEvent, MatLegacyPaginator as MatPaginator} from '@angular/material/legacy-paginator';
+import {MatLegacyTableDataSource as MatTableDataSource} from '@angular/material/legacy-table';
+import {TranslateService} from '@ngx-translate/core';
+import {tap} from 'rxjs/operators';
+import {PAGE_EVENT, TIMEOUT_ERROR} from 'src/app/core/constants/common.constant';
+import {PROPOSAL_STATUS, PROPOSAL_TYPE_COMMUNITY_POOL} from 'src/app/core/constants/proposal.constant';
+import {EnvironmentService} from 'src/app/core/data-services/environment.service';
+import {TableTemplate} from 'src/app/core/models/common.model';
+import {CommonService} from 'src/app/core/services/common.service';
+import {ProposalService} from 'src/app/core/services/proposal.service';
+import {shortenAddress} from 'src/app/core/utils/common/shorten';
+import {PaginatorComponent} from 'src/app/shared/components/paginator/paginator.component';
 
 @Component({
   selector: 'app-community-pool-proposal',
@@ -23,13 +23,13 @@ export class CommunityPoolProposalComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   templates: Array<TableTemplate> = [
-    { matColumnDef: 'id', headerCellDef: 'ID' },
-    { matColumnDef: 'title', headerCellDef: 'Title' },
-    { matColumnDef: 'status', headerCellDef: 'Status' },
-    { matColumnDef: 'sender', headerCellDef: 'Sender' },
-    { matColumnDef: 'recipient', headerCellDef: 'recipient' },
-    { matColumnDef: 'amount', headerCellDef: 'Amount' },
-    { matColumnDef: 'voting_end_time', headerCellDef: 'Voting End' },
+    {matColumnDef: 'id', headerCellDef: 'ID'},
+    {matColumnDef: 'title', headerCellDef: 'Title'},
+    {matColumnDef: 'status', headerCellDef: 'Status'},
+    {matColumnDef: 'sender', headerCellDef: 'Sender'},
+    {matColumnDef: 'recipient', headerCellDef: 'recipient'},
+    {matColumnDef: 'amount', headerCellDef: 'Amount'},
+    {matColumnDef: 'voting_end_time', headerCellDef: 'Voting End'},
   ];
   displayedColumns: string[] = this.templates.map((dta) => dta.matColumnDef);
   pageData: PageEvent = {
@@ -45,7 +45,7 @@ export class CommunityPoolProposalComponent implements OnInit {
         pageIndex: 1,
       };
 
-      this.getListProposal({ index: 1 });
+      this.getListProposal({index: 1});
     }),
   );
   length: number;
@@ -63,7 +63,8 @@ export class CommunityPoolProposalComponent implements OnInit {
     private layout: BreakpointObserver,
     private proposalService: ProposalService,
     public commonService: CommonService,
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     this.getAddressDistribution();
@@ -94,7 +95,7 @@ export class CommunityPoolProposalComponent implements OnInit {
     return resObj;
   }
 
-  getListProposal({ index }) {
+  getListProposal({index}) {
     let payload = {
       limit: this.pageData.pageSize,
       offset: (index - 1) * this.pageData.pageSize,
