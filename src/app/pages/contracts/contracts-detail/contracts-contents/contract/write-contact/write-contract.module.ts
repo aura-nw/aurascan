f74be53fdbbcb380b3add28748a6d147f0d6b466
nhphuc2411 <phucnguyen241193@gmail.com>
2023-12-20 10:10:10 +0700

    update translate privacy and policy

--- src/app/pages/contracts/contracts-detail/contracts-contents/contract/write-contact/write-contract.module.ts
+++ src/app/pages/contracts/contracts-detail/contracts-contents/contract/write-contact/write-contract.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { CustomPipeModule } from 'src/app/core/pipes/custom-pipe.module';
 import { MaterialModule } from 'src/app/material.module';
 import { TableNoDataModule } from 'src/app/shared/components/table-no-data/table-no-data.module';
@@ -10,7 +11,16 @@ import { WriteContractComponent } from './write-contract.component';
 
 @NgModule({
   declarations: [WriteContractComponent],
-  imports: [CommonModule, CustomPipeModule, TableNoDataModule, MaterialModule, RouterModule, FormsModule, SharedModule],
+  imports: [
+    CommonModule,
+    CustomPipeModule,
+    TableNoDataModule,
+    MaterialModule,
+    RouterModule,
+    FormsModule,
+    SharedModule,
+    TranslateModule,
+  ],
   exports: [WriteContractComponent],
 })
 export class WriteContractModule {}
