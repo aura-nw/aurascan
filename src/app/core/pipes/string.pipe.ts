4978b56a71e6742db60a5f4a0e220168bb7616c8
nhphuc2411 <phucnguyen241193@gmail.com>
2023-12-22 16:03:30 +0700

    update format

--- src/app/core/pipes/string.pipe.ts
+++ src/app/core/pipes/string.pipe.ts
@@ -1,19 +1,15 @@
-import {Pipe, PipeTransform} from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'ellipsis'})
 export class EllipsisPipe implements PipeTransform {
   transform(value: string, start: number, end: number = 0): string {
     const length = value?.length;
-
     if (length > start + end) {
       const firstPart = start ? value.slice(0, start) : '';
       const lastPart = end ? value.slice(length - end, length) : '';
-
       const middleText = start != 0 ? '...' : '';
-
       return `${firstPart}${middleText}${lastPart}`;
     }
-
     return value;
   }
 }
