1036fdb0a7667613cc93009f0f61a931a1875ca2
nttnguyen136 <nttnguyen136@gmail.com>
2024-02-19 09:46:59 +0700

    add keplr mobiles

--- src/app/core/utils/cosmoskit/helpers/browser.ts
+++ src/app/core/utils/cosmoskit/helpers/browser.ts
@@ -1,2 +1,3 @@
 export const isCoin98Browser = () => (window as any)?.coin98?.isMobile;
 export const isLeapBrowser = () => (window as any)?.leap?.mode?.includes('mobile');
+export const isKeplrBrowser = () => (window as any)?.keplr?.mode?.includes('mobile');
--- src/app/core/utils/cosmoskit/wallets/index.ts
+++ src/app/core/utils/cosmoskit/wallets/index.ts
@@ -1,11 +1,12 @@
 import { wallets as coin98Wallets } from '@cosmos-kit/coin98';
 import { MainWalletBase } from '@cosmos-kit/core';
 import { wallets as keplrWallets } from '@cosmos-kit/keplr-extension';
+import { wallets as keplrMobileWallets } from '@cosmos-kit/keplr-mobile';
 import { wallets as leapWallets } from '@cosmos-kit/leap-extension';
 import { wallets as leapSnapMetaMaskWallets } from '@cosmos-kit/leap-metamask-cosmos-snap';
 import { wallets as leapMobileWallets } from '@cosmos-kit/leap-mobile';
 import { LOGO_COIN98, LOGO_LEAP_METAMASK } from '../constant';
-import { isCoin98Browser, isLeapBrowser } from '../helpers/browser';
+import { isCoin98Browser, isKeplrBrowser, isLeapBrowser } from '../helpers/browser';
 import { wallets as coin98MobileWallets } from './coin98-mobile';
 import { wallets as wcWallets } from './wallet-connect/wc';
 
@@ -26,9 +27,14 @@ leapMobileWallets.forEach((lsw) => {
   lsw.walletInfo.prettyName = 'Leap';
 });
 
+keplrMobileWallets.forEach((lsw) => {
+  lsw.walletInfo.prettyName = 'Keplr';
+});
+
 const mobileWallets = [
   ...(isCoin98Browser() ? coin98Wallets : coin98MobileWallets),
   ...(isLeapBrowser() ? leapWallets : leapMobileWallets),
+  ...(isKeplrBrowser() ? keplrWallets : keplrMobileWallets),
 ] as MainWalletBase[];
 
 const desktopWallets = [
