e9d4afce91f987d81ddd5eac649ef59f3d816b77
nttnguyen136 <nttnguyen136@gmail.com>
2024-02-22 15:17:49 +0700

    fix mode

--- src/app/core/utils/cosmoskit/helpers/browser.ts
+++ src/app/core/utils/cosmoskit/helpers/browser.ts
@@ -1,8 +1,8 @@
 const anyWindow = window as any;
 
-export const isKeplrExtention = () => !!anyWindow?.keplr.mode.includes('extension');
+export const isKeplrExtention = () => !!anyWindow?.keplr?.mode?.includes('extension');
 export const isCoin98Extention = () => !!anyWindow?.coin98?.keplr?.isDesktop;
-export const isLeapExtention = () => !!anyWindow?.leap?.mode.includes('core');
+export const isLeapExtention = () => !!anyWindow?.leap?.mode?.includes('core');
 export const isMetamaskExtention = () => !!anyWindow?.ethereum?.isMetaMask;
 
 export const isCoin98Browser = () => !!anyWindow?.coin98?.isMobile;
