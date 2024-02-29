2140ada018d3fc94bc2d9c051f6df158db8b50e7
letrieuthien <ltthien001@gmail.com>
2024-02-29 09:16:37 +0700

    create evm-transaction-detail.component

--- src/app/pages/evm-transaction/evm-transaction-detail/evm-transaction-detail.component.ts
+++ src/app/pages/evm-transaction/evm-transaction-detail/evm-transaction-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-evm-transaction-detail',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./evm-transaction-detail.component.scss']
 })
 export class EvmTransactionDetailComponent {
+  loading = true;
+  transaction = null;
 
 }
