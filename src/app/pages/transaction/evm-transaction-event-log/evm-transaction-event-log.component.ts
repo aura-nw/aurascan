d3251c186679b2b73df523bddf799e7eb002f587
nttnguyen136 <nttnguyen136@gmail.com>
2024-03-04 17:22:48 +0700

    update evn tx event log

--- src/app/pages/transaction/evm-transaction-event-log/evm-transaction-event-log.component.ts
+++ src/app/pages/transaction/evm-transaction-event-log/evm-transaction-event-log.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-evm-transaction-event-log',
@@ -6,4 +6,33 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./evm-transaction-event-log.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EvmTransactionEventLogComponent {}
+export class EvmTransactionEventLogComponent implements OnInit {
+  @Input() eventLog: {
+    id: number;
+    contract?: string;
+    topics?: unknown[];
+    data?: unknown;
+  } = {
+    id: 1,
+    contract: 'XEN Crypto (0x2ab0e9e4ee70fff1fb9d67031e44f6410170d00e)',
+    topics: [
+      {
+        address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+        data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+      },
+      {
+        address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+        data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+      },
+      {
+        address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+        data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+      },
+    ],
+    data: {
+      value: '0 COXEN',
+      hexValue: '0000000000000000000000000000000000000000000000000000000000000000',
+    },
+  };
+  ngOnInit(): void {}
+}
--- src/app/pages/transaction/evm-transaction/evm-message/evm-message.component.ts
+++ src/app/pages/transaction/evm-transaction/evm-message/evm-message.component.ts
@@ -11,5 +11,74 @@ export class EvmMessageComponent {
   @Input() inputData: { [key: string]: unknown };
   @Input() eventLog: { [key: string]: unknown };
 
+  logs = [
+    {
+      id: 1,
+      contract: 'XEN Crypto (0x2ab0e9e4ee70fff1fb9d67031e44f6410170d00e)',
+      topics: [
+        {
+          address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+          data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+        },
+        {
+          address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+          data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+        },
+        {
+          address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+          data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+        },
+      ],
+      data: {
+        value: '0 COXEN',
+        hexValue: '0000000000000000000000000000000000000000000000000000000000000000',
+      },
+    },
+    {
+      id: 2,
+      contract: 'XEN Crypto (0x2ab0e9e4ee70fff1fb9d67031e44f6410170d00e)',
+      topics: [
+        {
+          address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+          data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+        },
+        {
+          address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+          data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+        },
+        {
+          address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+          data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+        },
+      ],
+      data: {
+        value: '0 COXEN',
+        hexValue: '0000000000000000000000000000000000000000000000000000000000000000',
+      },
+    },
+    {
+      id: 3,
+      contract: 'XEN Crypto (0x2ab0e9e4ee70fff1fb9d67031e44f6410170d00e)',
+      topics: [
+        {
+          address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+          data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+        },
+        {
+          address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+          data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+        },
+        {
+          address: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+          data: '> Transfer(address indexed from,address indexed to,uint256 value)',
+        },
+      ],
+      data: {
+        value: '0 COXEN',
+        hexValue: '0000000000000000000000000000000000000000000000000000000000000000',
+      },
+    },
+  ];
+
   showAll = false;
 }
