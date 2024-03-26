5302db8fff38baa655c5c501d77c92403c5e9684
letrieuthien <ltthien001@gmail.com>
2024-03-26 09:29:31 +0700

    format code

--- src/app/pages/profile/watchlist/popup-watchlist/popup-watchlist.component.ts
+++ src/app/pages/profile/watchlist/popup-watchlist/popup-watchlist.component.ts
@@ -1,19 +1,19 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import {
   MatLegacyDialogRef as MatDialogRef,
   MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA,
 } from '@angular/material/legacy-dialog';
-import {TranslateService} from '@ngx-translate/core';
-import {LENGTH_CHARACTER} from 'src/app/core/constants/common.constant';
-import {EnvironmentService} from 'src/app/core/data-services/environment.service';
-import {CommonService} from 'src/app/core/services/common.service';
-import {NameTagService} from 'src/app/core/services/name-tag.service';
-import {NgxToastrService} from 'src/app/core/services/ngx-toastr.service';
-import {WatchListService} from 'src/app/core/services/watch-list.service';
-import {isSafari} from 'src/app/core/utils/common/validation';
-import {EWalletType} from 'src/app/core/constants/wallet.constant';
-import {transferAddress} from 'src/app/core/utils/common/address-converter';
+import { TranslateService } from '@ngx-translate/core';
+import { LENGTH_CHARACTER } from 'src/app/core/constants/common.constant';
+import { EnvironmentService } from 'src/app/core/data-services/environment.service';
+import { CommonService } from 'src/app/core/services/common.service';
+import { NameTagService } from 'src/app/core/services/name-tag.service';
+import { NgxToastrService } from 'src/app/core/services/ngx-toastr.service';
+import { WatchListService } from 'src/app/core/services/watch-list.service';
+import { isSafari } from 'src/app/core/utils/common/validation';
+import { EWalletType } from 'src/app/core/constants/wallet.constant';
+import { transferAddress } from 'src/app/core/utils/common/address-converter';
 
 @Component({
   selector: 'app-popup-watchlist',
@@ -101,8 +101,7 @@ export class PopupWatchlistComponent implements OnInit {
     private toastr: NgxToastrService,
     private watchListService: WatchListService,
     private nameTagService: NameTagService,
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.isSafari = isSafari();
@@ -201,8 +200,7 @@ export class PopupWatchlistComponent implements OnInit {
       if (!JSON.stringify(this.settingObj)?.includes('true') && this.isTracking) {
         return false;
       }
-    } catch {
-    }
+    } catch {}
 
     this.formValid = true;
     return true;
@@ -210,7 +208,7 @@ export class PopupWatchlistComponent implements OnInit {
 
   onSubmit() {
     this.isSubmit = true;
-    const {favorite, address, evmAddress, note, id} = this.watchlistForm.getRawValue();
+    const { favorite, address, evmAddress, note, id } = this.watchlistForm.getRawValue();
 
     let payload = {
       address,
@@ -338,7 +336,7 @@ export class PopupWatchlistComponent implements OnInit {
       this.watchlistForm.get('address').disable();
     }
     if (!this.commonService.isValidContract(address)) {
-      const {accountAddress, accountEvmAddress} = transferAddress(
+      const { accountAddress, accountEvmAddress } = transferAddress(
         this.chainInfo.bech32Config.bech32PrefixAccAddr,
         address,
       );
