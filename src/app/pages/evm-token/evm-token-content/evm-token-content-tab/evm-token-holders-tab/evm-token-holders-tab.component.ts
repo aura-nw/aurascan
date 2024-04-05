3f266e599dff7a568e458b412b426bf5a8472b2a
nhphuc2411 <phucnguyen241193@gmail.com>
2024-04-05 16:39:12 +0700

    query holder ERC

--- src/app/pages/evm-token/evm-token-content/evm-token-content-tab/evm-token-holders-tab/evm-token-holders-tab.component.ts
+++ src/app/pages/evm-token/evm-token-content/evm-token-content-tab/evm-token-holders-tab/evm-token-holders-tab.component.ts
@@ -2,13 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { LegacyPageEvent as PageEvent } from '@angular/material/legacy-paginator';
 import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
 import { ActivatedRoute } from '@angular/router';
-import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
 import BigNumber from 'bignumber.js';
-import * as _ from 'lodash';
-import { map, of, switchMap } from 'rxjs';
-import { PAGE_EVENT, TIMEOUT_ERROR } from 'src/app/core/constants/common.constant';
-import { ContractRegisterType, EvmContractRegisterType } from 'src/app/core/constants/contract.enum';
-import { EModeToken } from 'src/app/core/constants/token.enum';
+import { map } from 'rxjs';
+import { PAGE_EVENT } from 'src/app/core/constants/common.constant';
+import { EvmContractRegisterType } from 'src/app/core/constants/contract.enum';
+import { EModeEvmToken } from 'src/app/core/constants/token.enum';
 import { EnvironmentService } from 'src/app/core/data-services/environment.service';
 import { TableTemplate } from 'src/app/core/models/common.model';
 import { CommonService } from 'src/app/core/services/common.service';
@@ -50,13 +48,13 @@ export class EvmTokenHoldersTabComponent implements OnInit {
   };
   loading = true;
   dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
-  contractType = ContractRegisterType;
+  contractType = EvmContractRegisterType;
   numberTopHolder = 100;
   totalQuantity = 0;
   numberTop = 0;
   totalHolder = 0;
   errTxt: string;
-  EModeToken = EModeToken;
+  EModeToken = EModeEvmToken;
   linkAddress: string;
   countTotal = 0;
 
@@ -79,112 +77,7 @@ export class EvmTokenHoldersTabComponent implements OnInit {
 
   getListData() {
     this.getDenomHolder();
-    // if (this.typeContract) {
-    //   if (this.typeContract !== EvmContractRegisterType.ERC20) {
-    //     this.getQuantity();
-    //   } else {
-    //     this.getHolder();
-    //   }
-    // }
   }
-
-  // getHolder() {
-  //   this.tokenService
-  //     .getListTokenHolder(
-  //       this.pageData.pageSize,
-  //       this.pageData.pageSize * this.pageData.pageIndex,
-  //       this.contractAddress,
-  //     )
-  //     .subscribe({
-  //       next: (res) => {
-  //         const data = _.get(res, `cw20_holder`);
-  //         const count = _.get(res, `cw20_holder_aggregate`);
-  //         if (data?.length > 0) {
-  //           this.totalHolder = count.aggregate?.count;
-  //           if (this.totalHolder > this.numberTopHolder) {
-  //             this.pageData.length = this.numberTopHolder;
-  //           } else {
-  //             this.pageData.length = this.totalHolder;
-  //           }
-  //           let dataFlat = data?.map((item) => {
-  //             return {
-  //               owner: item.address,
-  //               balance: item.amount,
-  //               percent_hold: BigNumber(item.amount)
-  //                 .dividedBy(BigNumber(item.cw20_contract.total_supply))
-  //                 .multipliedBy(100),
-  //               value:
-  //                 new BigNumber(item.amount)
-  //                   .multipliedBy(this.tokenDetail?.price)
-  //                   .dividedBy(BigNumber(10).pow(this.decimalValue))
-  //                   .toFixed() || 0,
-  //             };
-  //           });
-  //           this.dataSource = new MatTableDataSource<any>(dataFlat);
-  //         }
-  //       },
-  //       error: (e) => {
-  //         if (e.name === TIMEOUT_ERROR) {
-  //           this.errTxt = e.message;
-  //         } else {
-  //           this.errTxt = e.status + ' ' + e.statusText;
-  //         }
-  //         this.loading = false;
-  //       },
-  //       complete: () => {
-  //         this.loading = false;
-  //       },
-  //     });
-  // }
-
-  // getHolderNFT() {
-  //   const payload = {
-  //     limit: this.pageData.pageSize,
-  //     offset: this.pageData.pageSize * this.pageData.pageIndex,
-  //     contractAddress: this.contractAddress,
-  //   };
-  //   this.tokenService.getListTokenHolderNFT(payload).subscribe({
-  //     next: (res) => {
-  //       if (res?.view_count_holder_cw721?.length > 0) {
-  //         this.totalHolder = res.view_count_holder_cw721_aggregate?.aggregate?.count;
-  //         if (this.totalHolder > this.numberTopHolder) {
-  //           this.pageData.length = this.numberTopHolder;
-  //         } else {
-  //           this.pageData.length = this.totalHolder;
-  //         }
-
-  //         res?.view_count_holder_cw721.forEach((element) => {
-  //           element['quantity'] = element.count;
-  //         });
-
-  //         let topHolder = Math.max(...res?.view_count_holder_cw721.map((o) => o.quantity)) || 1;
-  //         this.numberTop = topHolder > this.numberTop ? topHolder : this.numberTop;
-  //         res?.view_count_holder_cw721.forEach((element) => {
-  //           element['value'] = 0;
-  //         });
-
-  //         if (this.totalQuantity) {
-  //           res?.view_count_holder_cw721.forEach((k) => {
-  //             k['percent_hold'] = (k.quantity / this.totalQuantity) * 100;
-  //             k['width_chart'] = (k.quantity / this.numberTop) * 100;
-  //           });
-  //         }
-  //         this.dataSource = new MatTableDataSource<any>(res.view_count_holder_cw721);
-  //       }
-  //     },
-  //     error: (e) => {
-  //       if (e.name === TIMEOUT_ERROR) {
-  //         this.errTxt = e.message;
-  //       } else {
-  //         this.errTxt = e.status + ' ' + e.statusText;
-  //       }
-  //       this.loading = false;
-  //     },
-  //     complete: () => {
-  //       this.loading = false;
-  //     },
-  //   });
-  // }
 
   compare(a: number | string, b: number | string, isAsc: boolean) {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
@@ -192,9 +85,6 @@ export class EvmTokenHoldersTabComponent implements OnInit {
 
   getTemplate(): Array<TableTemplate> {
     let result = this.ERC20Templates;
-    // if (this.typeContract && this.typeContract !== ContractRegisterType.CW20) {
-    //   result = this.CW721Templates;
-    // }
     return result;
   }
 
@@ -204,20 +94,8 @@ export class EvmTokenHoldersTabComponent implements OnInit {
 
   pageEvent(e: PageEvent): void {
     this.pageData.pageIndex = e.pageIndex;
-    // this.getListData();
+    this.getListData();
   }
-
-  // async getQuantity() {
-  //   let queryData = {
-  //     num_tokens: {},
-  //   };
-  //   const client = await SigningCosmWasmClient.connect(this.chainInfo.rpc);
-  //   try {
-  //     const config = await client.queryContractSmart(this.contractAddress, queryData);
-  //     this.totalQuantity = config?.count || 0;
-  //     this.getHolderNFT();
-  //   } catch (error) {}
-  // }
 
   getDenomHolder() {
     const payload = {
@@ -225,25 +103,25 @@ export class EvmTokenHoldersTabComponent implements OnInit {
       limit: this.pageData.pageSize,
       offset: this.pageData.pageIndex * this.pageData.pageSize,
       address: this.keyWord || null,
-      isExcludedAddresses: this.tokenDetail.modeToken === this.EModeToken.Native,
+      isExcludedAddresses: this.tokenDetail.modeToken === this.EModeToken.ERCToken,
     };
 
-    const tokenSupplyOrigin = BigNumber(this.tokenDetail?.totalSupply).multipliedBy(
+    const tokenSupplyOrigin = BigNumber(this.tokenDetail?.total_supply).multipliedBy(
       BigNumber(10).pow(this.tokenDetail.decimal),
     );
 
     this.tokenService
       .getDenomHolder(payload)
       .pipe(
-        switchMap((element) => {
+        map((element) => {
           if (element?.account_balance?.length === 0) {
-            return of([]);
+            return [];
           }
 
           this.totalHolder = element?.account_balance_aggregate?.aggregate?.count;
 
           let accountBalance = element['account_balance'];
-          if (this.tokenDetail.modeToken === this.EModeToken.IBCCoin) {
+          if (this.tokenDetail.modeToken === this.EModeToken.ERCToken) {
             accountBalance?.forEach((item) => {
               item.balance = item.amount;
               item.owner = item.account?.address;
@@ -254,35 +132,10 @@ export class EvmTokenHoldersTabComponent implements OnInit {
                   .dividedBy(BigNumber(10).pow(this.decimalValue))
                   .toFixed() || 0;
             });
-            return of(accountBalance);
+            return accountBalance;
           }
 
-          const addressList = accountBalance?.map((k) => {
-            return k.account?.address;
-          });
-
-          return this.tokenService.getListAmountNative(addressList).pipe(
-            map((res) => {
-              accountBalance?.forEach((item, index) => {
-                item.amount = item.balance = _.get(res.data[index], 'amount');
-                item.owner = item.account?.address;
-                item.percent_hold = BigNumber(item.amount).dividedBy(tokenSupplyOrigin).multipliedBy(100);
-                item.value =
-                  BigNumber(item.amount)
-                    .multipliedBy(this.tokenDetail?.price || 0)
-                    .dividedBy(BigNumber(10).pow(this.decimalValue))
-                    .toFixed() || 0;
-              });
-
-              if (accountBalance?.length == 1 && this.keyWord) {
-                this.tokenService.filterBalanceNative$.next(accountBalance[0]?.balance);
-              }
-
-              // sort list data with amount desc
-              const sortData = accountBalance?.sort((a, b) => this.compare(a.amount, b.amount, false));
-              return sortData;
-            }),
-          );
+          return [];
         }),
       )
       .subscribe({
