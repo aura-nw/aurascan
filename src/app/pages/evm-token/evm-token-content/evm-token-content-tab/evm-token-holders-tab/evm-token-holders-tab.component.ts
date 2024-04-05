375933dd8593dfce406b6293a47c58caf00a55e9
nhphuc2411 <phucnguyen241193@gmail.com>
2024-04-05 15:14:48 +0700

    draft

--- src/app/pages/evm-token/evm-token-content/evm-token-content-tab/evm-token-holders-tab/evm-token-holders-tab.component.ts
+++ src/app/pages/evm-token/evm-token-content/evm-token-content-tab/evm-token-holders-tab/evm-token-holders-tab.component.ts
@@ -26,7 +26,7 @@ export class EvmTokenHoldersTabComponent implements OnInit {
   @Input() tokenDetail: any;
   @Input() decimalValue: number;
 
-  CW20Templates: Array<TableTemplate> = [
+  ERC20Templates: Array<TableTemplate> = [
     { matColumnDef: 'id', headerCellDef: 'rank', headerWidth: 5 },
     { matColumnDef: 'owner', headerCellDef: 'address', headerWidth: 30 },
     { matColumnDef: 'balance', headerCellDef: 'amount', headerWidth: 12 },
@@ -34,12 +34,12 @@ export class EvmTokenHoldersTabComponent implements OnInit {
     { matColumnDef: 'value', headerCellDef: 'value', headerWidth: 12 },
   ];
 
-  CW721Templates: Array<TableTemplate> = [
-    { matColumnDef: 'id', headerCellDef: 'rank', headerWidth: 5 },
-    { matColumnDef: 'owner', headerCellDef: 'address', headerWidth: 40 },
-    { matColumnDef: 'quantity', headerCellDef: 'amount', headerWidth: 12 },
-    { matColumnDef: 'percent_hold', headerCellDef: 'percentage', headerWidth: 15 },
-  ];
+  // CW721Templates: Array<TableTemplate> = [
+  //   { matColumnDef: 'id', headerCellDef: 'rank', headerWidth: 5 },
+  //   { matColumnDef: 'owner', headerCellDef: 'address', headerWidth: 40 },
+  //   { matColumnDef: 'quantity', headerCellDef: 'amount', headerWidth: 12 },
+  //   { matColumnDef: 'percent_hold', headerCellDef: 'percentage', headerWidth: 15 },
+  // ];
 
   template: Array<TableTemplate> = [];
   displayedColumns: string[];
@@ -78,122 +78,123 @@ export class EvmTokenHoldersTabComponent implements OnInit {
   }
 
   getListData() {
-    if (this.typeContract) {
-      if (this.typeContract !== EvmContractRegisterType.ERC20) {
-        this.getQuantity();
-      } else {
-        this.getHolder();
-      }
-    }
+    this.getDenomHolder();
+    // if (this.typeContract) {
+    //   if (this.typeContract !== EvmContractRegisterType.ERC20) {
+    //     this.getQuantity();
+    //   } else {
+    //     this.getHolder();
+    //   }
+    // }
   }
 
-  getHolder() {
-    this.tokenService
-      .getListTokenHolder(
-        this.pageData.pageSize,
-        this.pageData.pageSize * this.pageData.pageIndex,
-        this.contractAddress,
-      )
-      .subscribe({
-        next: (res) => {
-          const data = _.get(res, `cw20_holder`);
-          const count = _.get(res, `cw20_holder_aggregate`);
-          if (data?.length > 0) {
-            this.totalHolder = count.aggregate?.count;
-            if (this.totalHolder > this.numberTopHolder) {
-              this.pageData.length = this.numberTopHolder;
-            } else {
-              this.pageData.length = this.totalHolder;
-            }
-            let dataFlat = data?.map((item) => {
-              return {
-                owner: item.address,
-                balance: item.amount,
-                percent_hold: BigNumber(item.amount)
-                  .dividedBy(BigNumber(item.cw20_contract.total_supply))
-                  .multipliedBy(100),
-                value:
-                  new BigNumber(item.amount)
-                    .multipliedBy(this.tokenDetail?.price)
-                    .dividedBy(BigNumber(10).pow(this.decimalValue))
-                    .toFixed() || 0,
-              };
-            });
-            this.dataSource = new MatTableDataSource<any>(dataFlat);
-          }
-        },
-        error: (e) => {
-          if (e.name === TIMEOUT_ERROR) {
-            this.errTxt = e.message;
-          } else {
-            this.errTxt = e.status + ' ' + e.statusText;
-          }
-          this.loading = false;
-        },
-        complete: () => {
-          this.loading = false;
-        },
-      });
-  }
+  // getHolder() {
+  //   this.tokenService
+  //     .getListTokenHolder(
+  //       this.pageData.pageSize,
+  //       this.pageData.pageSize * this.pageData.pageIndex,
+  //       this.contractAddress,
+  //     )
+  //     .subscribe({
+  //       next: (res) => {
+  //         const data = _.get(res, `cw20_holder`);
+  //         const count = _.get(res, `cw20_holder_aggregate`);
+  //         if (data?.length > 0) {
+  //           this.totalHolder = count.aggregate?.count;
+  //           if (this.totalHolder > this.numberTopHolder) {
+  //             this.pageData.length = this.numberTopHolder;
+  //           } else {
+  //             this.pageData.length = this.totalHolder;
+  //           }
+  //           let dataFlat = data?.map((item) => {
+  //             return {
+  //               owner: item.address,
+  //               balance: item.amount,
+  //               percent_hold: BigNumber(item.amount)
+  //                 .dividedBy(BigNumber(item.cw20_contract.total_supply))
+  //                 .multipliedBy(100),
+  //               value:
+  //                 new BigNumber(item.amount)
+  //                   .multipliedBy(this.tokenDetail?.price)
+  //                   .dividedBy(BigNumber(10).pow(this.decimalValue))
+  //                   .toFixed() || 0,
+  //             };
+  //           });
+  //           this.dataSource = new MatTableDataSource<any>(dataFlat);
+  //         }
+  //       },
+  //       error: (e) => {
+  //         if (e.name === TIMEOUT_ERROR) {
+  //           this.errTxt = e.message;
+  //         } else {
+  //           this.errTxt = e.status + ' ' + e.statusText;
+  //         }
+  //         this.loading = false;
+  //       },
+  //       complete: () => {
+  //         this.loading = false;
+  //       },
+  //     });
+  // }
 
-  getHolderNFT() {
-    const payload = {
-      limit: this.pageData.pageSize,
-      offset: this.pageData.pageSize * this.pageData.pageIndex,
-      contractAddress: this.contractAddress,
-    };
-    this.tokenService.getListTokenHolderNFT(payload).subscribe({
-      next: (res) => {
-        if (res?.view_count_holder_cw721?.length > 0) {
-          this.totalHolder = res.view_count_holder_cw721_aggregate?.aggregate?.count;
-          if (this.totalHolder > this.numberTopHolder) {
-            this.pageData.length = this.numberTopHolder;
-          } else {
-            this.pageData.length = this.totalHolder;
-          }
+  // getHolderNFT() {
+  //   const payload = {
+  //     limit: this.pageData.pageSize,
+  //     offset: this.pageData.pageSize * this.pageData.pageIndex,
+  //     contractAddress: this.contractAddress,
+  //   };
+  //   this.tokenService.getListTokenHolderNFT(payload).subscribe({
+  //     next: (res) => {
+  //       if (res?.view_count_holder_cw721?.length > 0) {
+  //         this.totalHolder = res.view_count_holder_cw721_aggregate?.aggregate?.count;
+  //         if (this.totalHolder > this.numberTopHolder) {
+  //           this.pageData.length = this.numberTopHolder;
+  //         } else {
+  //           this.pageData.length = this.totalHolder;
+  //         }
 
-          res?.view_count_holder_cw721.forEach((element) => {
-            element['quantity'] = element.count;
-          });
+  //         res?.view_count_holder_cw721.forEach((element) => {
+  //           element['quantity'] = element.count;
+  //         });
 
-          let topHolder = Math.max(...res?.view_count_holder_cw721.map((o) => o.quantity)) || 1;
-          this.numberTop = topHolder > this.numberTop ? topHolder : this.numberTop;
-          res?.view_count_holder_cw721.forEach((element) => {
-            element['value'] = 0;
-          });
+  //         let topHolder = Math.max(...res?.view_count_holder_cw721.map((o) => o.quantity)) || 1;
+  //         this.numberTop = topHolder > this.numberTop ? topHolder : this.numberTop;
+  //         res?.view_count_holder_cw721.forEach((element) => {
+  //           element['value'] = 0;
+  //         });
 
-          if (this.totalQuantity) {
-            res?.view_count_holder_cw721.forEach((k) => {
-              k['percent_hold'] = (k.quantity / this.totalQuantity) * 100;
-              k['width_chart'] = (k.quantity / this.numberTop) * 100;
-            });
-          }
-          this.dataSource = new MatTableDataSource<any>(res.view_count_holder_cw721);
-        }
-      },
-      error: (e) => {
-        if (e.name === TIMEOUT_ERROR) {
-          this.errTxt = e.message;
-        } else {
-          this.errTxt = e.status + ' ' + e.statusText;
-        }
-        this.loading = false;
-      },
-      complete: () => {
-        this.loading = false;
-      },
-    });
-  }
+  //         if (this.totalQuantity) {
+  //           res?.view_count_holder_cw721.forEach((k) => {
+  //             k['percent_hold'] = (k.quantity / this.totalQuantity) * 100;
+  //             k['width_chart'] = (k.quantity / this.numberTop) * 100;
+  //           });
+  //         }
+  //         this.dataSource = new MatTableDataSource<any>(res.view_count_holder_cw721);
+  //       }
+  //     },
+  //     error: (e) => {
+  //       if (e.name === TIMEOUT_ERROR) {
+  //         this.errTxt = e.message;
+  //       } else {
+  //         this.errTxt = e.status + ' ' + e.statusText;
+  //       }
+  //       this.loading = false;
+  //     },
+  //     complete: () => {
+  //       this.loading = false;
+  //     },
+  //   });
+  // }
 
   compare(a: number | string, b: number | string, isAsc: boolean) {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
 
   getTemplate(): Array<TableTemplate> {
-    let result = this.CW20Templates;
-    if (this.typeContract && this.typeContract !== ContractRegisterType.CW20) {
-      result = this.CW721Templates;
-    }
+    let result = this.ERC20Templates;
+    // if (this.typeContract && this.typeContract !== ContractRegisterType.CW20) {
+    //   result = this.CW721Templates;
+    // }
     return result;
   }
 
@@ -203,24 +204,24 @@ export class EvmTokenHoldersTabComponent implements OnInit {
 
   pageEvent(e: PageEvent): void {
     this.pageData.pageIndex = e.pageIndex;
-    this.getListData();
+    // this.getListData();
   }
 
-  async getQuantity() {
-    let queryData = {
-      num_tokens: {},
-    };
-    const client = await SigningCosmWasmClient.connect(this.chainInfo.rpc);
-    try {
-      const config = await client.queryContractSmart(this.contractAddress, queryData);
-      this.totalQuantity = config?.count || 0;
-      this.getHolderNFT();
-    } catch (error) {}
-  }
+  // async getQuantity() {
+  //   let queryData = {
+  //     num_tokens: {},
+  //   };
+  //   const client = await SigningCosmWasmClient.connect(this.chainInfo.rpc);
+  //   try {
+  //     const config = await client.queryContractSmart(this.contractAddress, queryData);
+  //     this.totalQuantity = config?.count || 0;
+  //     this.getHolderNFT();
+  //   } catch (error) {}
+  // }
 
   getDenomHolder() {
     const payload = {
-      denomHash: this.tokenDetail?.denomHash || this.tokenDetail?.denom,
+      denomHash: this.contractAddress,
       limit: this.pageData.pageSize,
       offset: this.pageData.pageIndex * this.pageData.pageSize,
       address: this.keyWord || null,
