f9f327199938c4d8380720a3cbe4669ec2465a09
Huynh Van Son <hvson96@gmail.com>
2024-05-17 13:14:51 +0700

    [feat][557] account detail ERC721 nfts (#3392)

--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -128,7 +128,7 @@ export class AccountService extends CommonService {
       .pipe(map((res) => (res?.data ? res?.data[this.envDB] : null)));
   }
 
-  getListCollectionByOwner(payload): Observable<any> {
+  getListCw721CollectionByOwner(payload): Observable<any> {
     const operationsDoc = `
     query queryListCollection($owner: String = null)  {
       ${this.envDB} {
@@ -158,7 +158,7 @@ export class AccountService extends CommonService {
       .pipe(map((res) => (res?.data ? res?.data[this.envDB] : null)));
   }
 
-  countListCollectionByOwner(payload): Observable<any> {
+  countListCw721CollectionByOwner(payload): Observable<any> {
     const operationsDoc = `
     query queryListCollection($owner: String = null)  {
       ${this.envDB} {
@@ -177,6 +177,159 @@ export class AccountService extends CommonService {
           owner: payload?.owner,
         },
         operationName: 'queryListCollection',
+      })
+      .pipe(map((res) => (res?.data ? res?.data[this.envDB] : null)));
+  }
+
+  getAssetERC721ByOwner(payload): Observable<any> {
+    if (payload.keyword?.length >= LENGTH_CHARACTER.ADDRESS) {
+      payload.contractAddress = payload.keyword;
+    } else if (payload.keyword?.length > 0) {
+      payload.token_id = payload.keyword;
+    }
+    const operationsDoc = `
+    query queryAssetERC721(
+      $contract_address: String
+      $limit: Int = 10
+      $tokenId: String = null
+      $owner: String = null
+      $offset: Int = 0
+    ) {
+      ${this.envDB} {
+        cw721_token: erc721_token(
+          limit: $limit
+          offset: $offset
+          where: {
+            erc721_contract: {
+              evm_smart_contract: { address: { _eq: $contract_address } }
+            }
+            token_id: { _eq: $tokenId }
+            owner: { _eq: $owner }
+          }
+          order_by: [{ last_updated_height: desc }, { id: desc }]
+        ) {
+          id
+          token_id
+          owner
+          media_info
+          last_updated_height
+          created_at
+          cw721_contract: erc721_contract {
+            name
+            symbol
+            smart_contract: evm_smart_contract {
+              address
+            }
+          }
+        }
+      }
+    }
+    `;
+    return this.http
+      .post<any>(this.graphUrl, {
+        query: operationsDoc,
+        variables: {
+          limit: payload?.limit || 20,
+          offset: payload.offset,
+          contract_address: payload?.contractAddress || payload?.address,
+          nextKeyLastUpdatedHeight: payload?.nextKey,
+          nextKeyId: payload?.nextKeyId,
+          tokenId: payload?.token_id,
+          owner: payload?.owner,
+        },
+        operationName: 'queryAssetERC721',
+      })
+      .pipe(map((res) => (res?.data ? res?.data[this.envDB] : null)));
+  }
+
+  countAssetERC721ByOwner(payload): Observable<any> {
+    if (payload.keyword?.length >= LENGTH_CHARACTER.ADDRESS) {
+      payload.contractAddress = payload.keyword;
+    } else if (payload.keyword?.length > 0) {
+      payload.token_id = payload.keyword;
+    }
+    const operationsDoc = `
+    query queryAssetERC721(
+      $contract_address: String
+      $tokenId: String = null
+      $owner: String = null
+    ) {
+      ${this.envDB} {
+        cw721_token_aggregate: erc721_token_aggregate(
+          where: {erc721_contract: {evm_smart_contract: {address: {_eq: $contract_address}}}, token_id: {_eq: $tokenId}, owner: {_eq: $owner}}
+          order_by: [{last_updated_height: desc}, {id: desc}]
+        ) {
+          aggregate {
+            count
+          }
+        }
+      }
+    }
+    `;
+    return this.http
+      .post<any>(this.graphUrl, {
+        query: operationsDoc,
+        variables: {
+          contract_address: payload?.contractAddress || payload?.address,
+          nextKeyLastUpdatedHeight: payload?.nextKey,
+          nextKeyId: payload?.nextKeyId,
+          tokenId: payload?.token_id,
+          owner: payload?.owner,
+        },
+        operationName: 'queryAssetERC721',
+      })
+      .pipe(map((res) => (res?.data ? res?.data[this.envDB] : null)));
+  }
+
+  getListERC721CollectionByOwner(payload): Observable<any> {
+    const operationsDoc = `
+    query queryListERC721Collection($owner: String = null)  {
+      ${this.envDB} {
+        cw721_contract: erc721_contract(where: {erc721_tokens: {owner: {_eq: $owner}}}, order_by: {name: asc}) {
+          name
+          symbol
+          cw721_tokens_aggregate: erc721_tokens_aggregate(where: {owner: {_eq: $owner}}) {
+            aggregate {
+              count
+            }
+          }
+          smart_contract: evm_smart_contract {
+            address
+          }
+        }
+      }
+    }
+    `;
+    return this.http
+      .post<any>(this.graphUrl, {
+        query: operationsDoc,
+        variables: {
+          owner: payload?.owner,
+        },
+        operationName: 'queryListERC721Collection',
+      })
+      .pipe(map((res) => (res?.data ? res?.data[this.envDB] : null)));
+  }
+
+  countListERC721CollectionByOwner(payload): Observable<any> {
+    const operationsDoc = `
+    query queryListERC721Collection($owner: String = null)  {
+      ${this.envDB} {
+        cw721_token_aggregate: erc721_token_aggregate(where: {owner: {_eq: $owner}}) {
+          aggregate {
+            count
+          }
+        }
+      }
+    }
+    `;
+    return this.http
+      .post<any>(this.graphUrl, {
+        query: operationsDoc,
+        variables: {
+          owner: payload?.owner,
+        },
+        operationName: 'queryListERC721Collection',
       })
       .pipe(map((res) => (res?.data ? res?.data[this.envDB] : null)));
   }
