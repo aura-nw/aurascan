f9c83b41661879c45e9d9561c549b388e16d7eb8
Son Huynh <hvson96@gmail.com>
2024-06-04 06:59:57 +0700

    [Feat][533] Display EVM address both upper case & normal case

--- src/app/core/pipes/evm-address.pipe.ts
+++ src/app/core/pipes/evm-address.pipe.ts
@@ -1,11 +1,15 @@
 import { Pipe, type PipeTransform } from '@angular/core';
 import { getEvmChecksumAddress } from '../utils/common/address-converter';
+import { EWalletType } from '../constants/wallet.constant';
 
 @Pipe({
-  name: 'appEvmAddress',
+  name: 'beautyAddress',
 })
 export class EvmAddressPipe implements PipeTransform {
   transform(value: string): string {
+    console.log(value, getEvmChecksumAddress(value));
+    if (!value?.startsWith(EWalletType.EVM)) return value;
+
     return getEvmChecksumAddress(value);
   }
 }
