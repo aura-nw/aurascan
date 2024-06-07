fc4c0a280ab79580301d034bd37df48c2225bb13
tamvb2391 <33695595+tamvb2391@users.noreply.github.com>
2024-06-07 18:11:58 +0700

    [FeatFix][582] - validate bool type for read/write contract (#3484)
    [FeatFix][611] - fix switch chain

--- src/app/core/utils/ethers/validate.ts
+++ src/app/core/utils/ethers/validate.ts
@@ -4,8 +4,13 @@ export function validateAndParsingInput(type: JsonFragmentType, value: any) {
   switch (type.type) {
     case 'uint256':
     case 'bytes4':
-      return value;
+      return { value };
+    case 'bool':
+      if (value?.toLowerCase() !== 'true' && value?.toLowerCase() !== 'false') {
+        return { value, error: `"${value}" is an invalid parameter, please use true/false.` };
+      }
+      return { value: value?.toLowerCase() === 'true' ? true : false };
     default:
-      return value;
+      return { value };
   }
 }
