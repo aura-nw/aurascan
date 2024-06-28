5adaaadf0e79fd74a847bf6542bf6aa44a6731cd
Tran Trung Tien <trantrungtien9x@gmail.com>
2024-06-28 14:48:28 +0700

    Apply decode ui

--- src/app/pages/transaction/evm-transaction/decode-message/decode-message.component.ts
+++ src/app/pages/transaction/evm-transaction/decode-message/decode-message.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { EnvironmentService } from 'src/app/core/data-services/environment.service';
 
 @Component({
   selector: 'app-decode-message',
@@ -7,25 +8,30 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DecodeMessageComponent implements OnInit {
   @Input() index: number | string;
+  @Input() isLink?: boolean;
   @Input() name?: string;
   @Input() isAllowSwitchDecode?: boolean;
   @Input() value: string;
   @Input() decode: string;
   @Input() isHighlight?: boolean;
-  
+
   data = '';
   type: 'Decode' | 'Hex' = 'Hex';
+  isMobile = false;
+
+  constructor(private environmentService: EnvironmentService) {}
 
   ngOnInit(): void {
-    this.data = this.value
+    this.isMobile = this.environmentService.isMobile;
+    this.data = this.value;
   }
   onDecode() {
     this.type = 'Decode';
-    this.data = this.decode
+    this.data = this.decode;
   }
 
   onHex() {
     this.type = 'Hex';
-    this.data = this.value
+    this.data = this.value;
   }
 }
