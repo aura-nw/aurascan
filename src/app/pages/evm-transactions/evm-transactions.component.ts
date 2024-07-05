15ff3dd5a68fad7b53d631714d4dea53e017b668
Tran Trung Tien <trantrungtien9x@gmail.com>
2024-07-05 17:11:07 +0700

    fix: disable decode when transaction is send with memo

--- src/app/pages/evm-transactions/evm-transactions.component.ts
+++ src/app/pages/evm-transactions/evm-transactions.component.ts
@@ -113,8 +113,6 @@ export class EvmTransactionsComponent {
       )
       .subscribe({
         next: (res) => {
-          console.log({res});
-          
           this.dataSource.data = res;
         },
         error: (e) => {
