ab24586bbbb9d901ea5e6b36a3521e70b5d89ee9
Tran Trung Tien <trantrungtien9x@gmail.com>
2024-07-11 18:29:44 +0700

    fix event log

--- src/app/pages/transaction/evm-transaction/decode-message/decode-message.component.ts
+++ src/app/pages/transaction/evm-transaction/decode-message/decode-message.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { EnvironmentService } from 'src/app/core/data-services/environment.service';
+import { ContractService } from 'src/app/core/services/contract.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-decode-message',
@@ -16,53 +18,101 @@ export class DecodeMessageComponent implements OnInit {
   @Input() isHighlight?: boolean;
   @Input() isDataField?: boolean;
 
-  data: string | any[] = '';
+  data: string | any = '';
   type: 'Decode' | 'Hex' = 'Hex';
   isMobile = false;
-
-  constructor(private environmentService: EnvironmentService) {}
+  isEvmContract = false;
+  constructor(
+    private environmentService: EnvironmentService,
+    private contractService: ContractService,
+    private detectRef: ChangeDetectorRef,
+  ) {}
 
   ngOnInit(): void {
     this.isMobile = this.environmentService.isMobile;
     this.data = this.value;
   }
+
+  checkEvmContract(address: string, cb: (isContract: boolean) => void) {
+    this.contractService.findEvmContract(address).subscribe({
+      next: (res) => {
+        if (res?.evm_smart_contract?.length > 0) cb(true);
+        else cb(false);
+      },
+    });
+  }
+
+  getAddress (data: any[]) {
+    const addresses = [];
+    for (const item of data) {
+      if(item?.isLink) addresses.push(item?.decode);
+      else if(item?.type === 'tuple') {
+        item?.decode?.split(',')?.forEach((tupleItem) => {
+          if (tupleItem?.startsWith('0x')) {
+            addresses.push(tupleItem);
+          }})
+      } 
+    }
+    return _.uniq(addresses.filter(Boolean));
+  }
+
   onDecode(field?: string) {
     this.type = 'Decode';
-    if(field !== 'data'){
+    if (field !== 'data') {
       this.data = this.decode;
+      this.checkEvmContract(this.decode, (isContract) => {
+        if (isContract) this.isEvmContract = true;
+        else this.isEvmContract = false;
+        this.detectRef.detectChanges();
+      });
       return;
+    } else {
+      if (Array.isArray(this.decode)) {
+        const addresses = this.getAddress(this.decode);       
+        this.contractService.findEvmContractList(addresses).subscribe({
+          next: (evmList) => {
+            this.data = Array.isArray(this.decode) && this.decode?.map((item) => {
+              if (item?.type !== 'tuple') {
+                const isEvmContract = !!evmList?.evm_smart_contract?.find(contract => contract?.address === item?.decode?.toString()?.toLowerCase());
+
+                return {
+                  ...item,
+                  isEvmContract,
+                  isArray: false,
+                };
+              }
+              if (item?.type === 'tuple') {
+                const links = item?.decode?.split(',')?.map((tupleItem: string) => {
+                  if (tupleItem?.startsWith('0x')) {
+                  const isEvmContract = !!evmList?.evm_smart_contract?.find(contract => contract?.address === tupleItem?.toString()?.toLowerCase());
+                    return {
+                      isEvmContract,
+                      name: tupleItem,
+                      isLink: true,
+                    };
+                  }
+                  return {
+                    name: tupleItem,
+                    isLink: false,
+                  };
+                });
+                return {
+                  ...item,
+                  isArray: true,
+                  decode: links,
+                };
+              }
+            });
+            this.detectRef.detectChanges();
+          },
+        })
+      }
     }
-    this.data = Array.isArray(this.decode) && this.decode?.map(item => {
-      if(item?.type !== 'tuple') {
-        return {
-          ...item,
-          isArray: false,
-        }
-      }
-      if(item?.type === 'tuple') {
-        const links = item?.decode?.split(',')?.map(item => {
-          if(item?.startsWith('0x')) {
-            return {
-              name: item,
-              isLink: true,
-            }
-          }
-          return {
-            name : item,
-            isLink : false
-          }
-        });
-        return {
-          ...item,
-          isArray: true,
-          decode: links
-        }
-      }
-    })
   }
 
   onHex() {
     this.type = 'Hex';
     this.data = this.value;
+    this.isEvmContract = false;
   }
 }
