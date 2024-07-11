60f467d5536f730111e6cc66188a69561fecbe87
Tran Trung Tien <trantrungtien9x@gmail.com>
2024-07-11 18:29:44 +0700

    fix event log

--- src/app/core/helpers/global-error.ts
+++ src/app/core/helpers/global-error.ts
@@ -4,6 +4,8 @@ import { ErrorHandler, Injectable } from '@angular/core';
 export class GlobalErrorHandler implements ErrorHandler {
 
   handleError(error: any): void {
+    console.log(error);
+    
    const chunkFailedMessage = /Loading chunk [\d]+ failed/;
 
     if (chunkFailedMessage.test(error?.message)) {
