ca26727459fbfb8abe3ba23b6440151ad54e6d46
Tran Trung Tien <trantrungtien9x@gmail.com>
2024-07-12 18:58:10 +0700

    update responsive for mobile

--- src/app/core/helpers/global-error.ts
+++ src/app/core/helpers/global-error.ts
@@ -4,8 +4,6 @@ import { ErrorHandler, Injectable } from '@angular/core';
 export class GlobalErrorHandler implements ErrorHandler {
 
   handleError(error: any): void {
-    console.log(error);
-    
    const chunkFailedMessage = /Loading chunk [\d]+ failed/;
 
     if (chunkFailedMessage.test(error?.message)) {
