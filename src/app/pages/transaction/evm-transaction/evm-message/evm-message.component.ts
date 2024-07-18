de81bdfd202c8f5d7e1c4141c5cf435bb824721c
Tran Trung Tien <trantrungtien9x@gmail.com>
2024-07-18 10:26:36 +0700

    update highlight function

--- src/app/pages/transaction/evm-transaction/evm-message/evm-message.component.ts
+++ src/app/pages/transaction/evm-transaction/evm-message/evm-message.component.ts
@@ -222,12 +222,13 @@ export class EvmMessageComponent {
 
   getFunctionColor(inputs: any[]) {
     return inputs?.map((i, index) => {
-      const { type, indexed, name } = i;
-      let param = '';
-      if(type) param += type;
-      if(indexed) param += ' indexed';
-      if(name) param += ` ${name}`;
-
+      const { type, indexed, name, components} = i;
+      if (type === 'tuple') {
+        const paramHTML = `<span style="color: var(--aura-green-3); font-family: inherit">${components?.map(this.mappingFunctionName)?.join(', ')}</span>`
+        const tupleType = `(${paramHTML}) ${name}`;
+        console.log(tupleType);
+        
+      }
       const dataTypeHTML = type
         ? `<span style="color: var(--aura-green-3); font-family: inherit">${type}</span>`
         : '';
@@ -237,8 +238,13 @@ export class EvmMessageComponent {
           }; font-family: inherit">indexed</span>`
         : '';
       const nameHTML = name ? `<span style="color: var(--aura-red-3); font-family: inherit">${name}</span>` : '';
+
+      let param = index > 0 ? ' ' : '';
+      if(type) param += dataTypeHTML;
+      if(indexed) param += ` ${modifierHTML}`;
+      if(name) param += ` ${nameHTML}`;
       
-      return `${index > 0 ? ' ' : ''}${dataTypeHTML} ${modifierHTML} ${nameHTML}`;
+      return param;
     })
   }
 
@@ -261,9 +267,13 @@ export class EvmMessageComponent {
 
           if (!paramsDecode) decoded = this.mappingTopics(element);
           else {
+            
+            const paramsHTML = this.getFunctionColor(paramsDecode?.fragment?.inputs);
+            
             const params = paramsDecode?.fragment?.inputs.map(this.mappingFunctionName);
-            const decodeTopic0 = `> ${paramsDecode?.fragment?.name}(${params.join(', ')})`;
-
+            
+            const decodeTopic0 = `> ${paramsDecode?.fragment?.name}(${paramsHTML})`;
+            
             decoded = [
               {
                 index: 0,
@@ -280,10 +290,6 @@ export class EvmMessageComponent {
 
               inputs?.forEach((item, idx) => {
                 if (item?.type === 'tuple') {
-                  const tupleType = `(${item?.components?.map(this.mappingFunctionName)?.join(', ')}) ${item?.name}`;
-                  const replaceTuple = new RegExp(`\\b${item?.type} ${item?.name}\\b`, 'g');
-                  decoded[0].decode = decoded[0]?.decode?.replace(replaceTuple, tupleType);
-
                   item?.components?.forEach((tupleParam, tidx) => {
                     const tupleDecode = {
                       indexed: tupleParam?.indexed,
@@ -308,7 +314,8 @@ export class EvmMessageComponent {
                   param['indexed'] = item.indexed;
                   param['index'] = currentParamIndex + 1;
                   param['isAllowSwitchDecode'] = true;
-                  (param['value'] = element.topics[currentParamIndex + 1]), (currentParamIndex += 1);
+                  param['value'] = element.topics[currentParamIndex + 1];
+                  currentParamIndex += 1;
                   params.push(param);
                 } else {
                   data.push(param);
