d5727dd1470d88957ff0ab4d4e60d88d5b2f83cc
CloudCludfore <CloudCludfore@gmail.com>
2025-02-20 16:59:26 +0700

    update

--- src/app/core/utils/cosmoskit/chains.ts
+++ src/app/core/utils/cosmoskit/chains.ts
@@ -2,9 +2,8 @@ import { chains } from 'chain-registry';
 import Devnet from './sample/dev/dev.json';
 import Euphoria from './sample/euphoria/euphoria.json';
 import Serenity from './sample/serenity/serenity.json';
-import Main from './sample/main/main.json';
 
-const testnetChains = [Devnet, Serenity, Euphoria, Main];
+const testnetChains = [Devnet, Serenity, Euphoria];
 const allChains = [...chains, ...testnetChains];
 
 export { allChains };
